d747ea88b6e13508be5467c62fd2062b29f78664
Uy Ha <hchanuy@gmail.com>
2020-08-13 10:08:16 +0200

    Stop using miniconda

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -12,8 +12,5 @@ function d {
   sl D:\
 }
 
-# Conda
-(& "D:\Programming\Miniconda\3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
-
 # Use Starship as prompt
 Invoke-Expression (&starship init powershell)
