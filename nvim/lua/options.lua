0646f5eb00051fbe43b9d2ed2b3c1c98d322e44e
Uy Ha <hchanuy@gmail.com>
2021-02-25 18:43:11 +0100

    [WIP]: Migrating to init.lua: - Move setting options to lua/options.lua - Add more plugins

---
+++ nvim/lua/options.lua
@@ -0,0 +1,37 @@
+local scopes = {o = vim.o, b = vim.bo, w = vim.wo}
+local opt = function(scope, key, value)
+  scopes[scope][key] = value
+  scopes['o'][key] = value
+end
+
+local indent = 2
+local width = 120
+
+opt('b', 'expandtab', true)               -- Use spaces instead of tabs
+opt('b', 'formatoptions', 'crqnj')        -- Automatic formatting options
+opt('b', 'shiftwidth', indent)            -- Size of an indent
+opt('b', 'smartindent', true)             -- Insert indents automatically
+opt('b', 'tabstop', indent)               -- Number of spaces tabs count for
+opt('b', 'textwidth', width)              -- Maximum width of text
+opt('o', 'completeopt', 'menuone,noinsert,noselect')  -- Completion options
+opt('o', 'hidden', true)                  -- Enable background buffers
+opt('o', 'ignorecase', true)              -- Ignore case
+opt('o', 'joinspaces', false)             -- No double spaces with join
+opt('o', 'pastetoggle', '<F2>')           -- Paste mode
+opt('o', 'scrolloff', 4 )                 -- Lines of context
+opt('o', 'shiftround', true)              -- Round indent
+opt('o', 'sidescrolloff', 8 )             -- Columns of context
+opt('o', 'smartcase', true)               -- Don't ignore case with capitals
+opt('o', 'splitbelow', true)              -- Put new windows below current
+opt('o', 'splitright', true)              -- Put new windows right of current
+opt('o', 'termguicolors', true)           -- True color support
+opt('o', 'updatetime', 200)               -- Delay before swap file is saved
+opt('o', 'wildmode', 'list:longest')      -- Command-line completion mode
+opt('w', 'colorcolumn', tostring(width))  -- Line length marker
+opt('w', 'cursorline', true)              -- Highlight cursor line
+opt('w', 'list', true)                    -- Show some invisible characters
+opt('w', 'number', true)                  -- Show line numbers
+opt('w', 'relativenumber', true)          -- Relative line numbers
+opt('w', 'signcolumn', 'yes')             -- Show sign column
+opt('w', 'wrap', false)                   -- Disable line wrap
+
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -6,5 +6,10 @@ local use = packer.use
 
 packer.init()
 
-use {'wbthomason/packer.nvim', opt = true}
+use {'PProvost/vim-ps1'}
+use {'cespare/vim-toml'}
 use {'joshdick/onedark.vim'}
+use {'tpope/vim-commentary'}
+use {'tpope/vim-fugitive'}
+use {'tpope/vim-surround'}
+use {'wbthomason/packer.nvim', opt = true}
