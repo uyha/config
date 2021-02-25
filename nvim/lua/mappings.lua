b98f883720c798a84c8201ed470f21d2ac2b66d3
Uy Ha <hchanuy@gmail.com>
2021-02-26 00:31:32 +0100

    Move mapleader definition to variables.lua

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,10 +1,7 @@
-local g = vim.g
-
 local map = function(mode, lhs, rhs, opts)
   local options = {noremap = true}
   if opts then options = vim.tbl_extend('force', options, opts) end
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
-g.mapleader = ' '
 map('', '<leader>c', '"+y')
