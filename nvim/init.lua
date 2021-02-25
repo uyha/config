649bbd17febe1680edc04f0f0f208930a0bb4a3a
Uy Ha <hchanuy@gmail.com>
2021-02-25 18:43:11 +0100

    [WIP]: Migrating to init.lua: - Move setting options to lua/options.lua - Add more plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,5 @@
 -- Adapted from https://github.com/ojroques/dotfiles/blob/master/nvim/init.lua
 local api, cmd, fn, g = vim.api, vim.cmd, vim.fn, vim.g
-local scopes = {o = vim.o, b = vim.bo, w = vim.wo}
 
 local map = function(mode, lhs, rhs, opts)
   local options = {noremap = true}
@@ -8,42 +7,10 @@ local map = function(mode, lhs, rhs, opts)
   api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
-local opt = function(scope, key, value)
-  scopes[scope][key] = value
-  scopes['o'][key] = value
-end
 
-require('plugins')
+require 'plugins'
+require 'options'
 
-cmd 'lang en'
+cmd 'lang en_US.utf8'
 cmd 'colorscheme onedark'
 
-local indent = 2
-local width = 120
-opt('b', 'expandtab', true)               -- Use spaces instead of tabs
-opt('b', 'formatoptions', 'crqnj')        -- Automatic formatting options
-opt('b', 'shiftwidth', indent)            -- Size of an indent
-opt('b', 'smartindent', true)             -- Insert indents automatically
-opt('b', 'tabstop', indent)               -- Number of spaces tabs count for
-opt('b', 'textwidth', width)              -- Maximum width of text
-opt('o', 'completeopt', 'menuone,noinsert,noselect')  -- Completion options
-opt('o', 'hidden', true)                  -- Enable background buffers
-opt('o', 'ignorecase', true)              -- Ignore case
-opt('o', 'joinspaces', false)             -- No double spaces with join
-opt('o', 'pastetoggle', '<F2>')           -- Paste mode
-opt('o', 'scrolloff', 4 )                 -- Lines of context
-opt('o', 'shiftround', true)              -- Round indent
-opt('o', 'sidescrolloff', 8 )             -- Columns of context
-opt('o', 'smartcase', true)               -- Don't ignore case with capitals
-opt('o', 'splitbelow', true)              -- Put new windows below current
-opt('o', 'splitright', true)              -- Put new windows right of current
-opt('o', 'termguicolors', true)           -- True color support
-opt('o', 'updatetime', 200)               -- Delay before swap file is saved
-opt('o', 'wildmode', 'list:longest')      -- Command-line completion mode
-opt('w', 'colorcolumn', tostring(width))  -- Line length marker
-opt('w', 'cursorline', true)              -- Highlight cursor line
-opt('w', 'list', true)                    -- Show some invisible characters
-opt('w', 'number', true)                  -- Show line numbers
-opt('w', 'relativenumber', true)          -- Relative line numbers
-opt('w', 'signcolumn', 'yes')             -- Show sign column
-opt('w', 'wrap', false)                   -- Disable line wrap
