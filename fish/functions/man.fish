94a081a04c1d71a6fea46bb982be6ac701c6737b
Uy Ha <hchanuy@gmail.com>
2021-02-25 11:11:42 +0100

    No need for man to open browser

--- fish/functions/man.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function man --wraps=man\ -H\"firefox\ -P\ \'default\'\" --description alias\ man=man\ -H\"firefox\ -P\ \'default\'\"
- command man -H"firefox -P 'default'" $argv;
-end
