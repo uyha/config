ad6761f6030b18bd2830244f586b0299cff0b575
Uy Ha <hchanuy@gmail.com>
2021-02-26 17:26:02 +0100

    Rearrange require statements

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,11 @@
 -- Adapted from https://github.com/ojroques/dotfiles/blob/master/nvim/init.lua
-require 'lsp'
-require 'mappings'
+
 require 'options'
+require 'variables'       --'variables' needs to be loaded before 'mapping'
 require 'plugins'
+require 'mappings'
 require 'treesitter'
-require 'variables'
+require 'lsp'
 
 local cmd = vim.cmd
 cmd 'lang en_US.utf8'
