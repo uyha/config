abc9ff0fab20b067f4cc2cb125d16171337a900b
Uy Ha <hchanuy@gmail.com>
2021-02-26 16:33:46 +0100

    Enable treesitter indent and incremental_selection

--- nvim/lua/treesitter.lua
+++ nvim/lua/treesitter.lua
@@ -2,5 +2,11 @@ require 'nvim-treesitter.configs'.setup{
   ensure_install = "maintained",
   highlight = {
     enable = true
+  },
+  indent = {
+    enable = true
+  },
+  incremental_selection = {
+    enable = true
   }
 }
