f9305c1ffba8af6bdbae0450aaabd1e751a5afe8
Uy Ha <hchanuy@gmail.com>
2021-02-26 16:35:56 +0100

    Clean up init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,13 +1,12 @@
 -- Adapted from https://github.com/ojroques/dotfiles/blob/master/nvim/init.lua
-local cmd = vim.cmd
-
-require 'options'
-require 'variables'
-require 'mappings'
-require 'plugins'
 require 'lsp'
+require 'mappings'
+require 'options'
+require 'plugins'
 require 'treesitter'
+require 'variables'
 
+local cmd = vim.cmd
 cmd 'lang en_US.utf8'
 cmd 'colorscheme onedark'
 
