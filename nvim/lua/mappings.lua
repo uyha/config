8ba0f5643e11bc0a329c755d126f32b02bcea59d
Uy Ha <hchanuy@gmail.com>
2021-02-27 23:46:06 +0100

    Neovim: Move mappings to mappings.lua

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,3 +1,5 @@
 local map = require 'utils'.map
 map('', '<leader>y', '"+y')
 map('', '<leader>p', '"+p')
+map('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<S-Tab>"', {expr = true})
+map('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', {expr = true})
