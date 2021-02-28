8bbddba7f01fd7c1fe8c2a1160e3e27f01fc37c2
Uy Ha <hchanuy@gmail.com>
2021-02-28 12:20:57 +0100

    Neovim: configure UltiSnippets

--- nvim/lua/variables.lua
+++ nvim/lua/variables.lua
@@ -2,4 +2,5 @@ local g = vim.g
 
 g.rainbow_active = true
 g.mapleader = ' '
--- g.completion_enable_snippet = 'UltiSnips'
+g.completion_enable_snippet = 'UltiSnips'
+g.UltiSnipsSnippetDirectories = {"ultisnippets"}
