dbd2fdc530586c438d91a043a0333e4e74151d6c
Uy Ha <hchanuy@gmail.com>
2021-02-28 13:00:19 +0100

    Neovim: configure UltiSnips: - Use <c-e> for expanding snippets - Add cpp snippets

--- nvim/lua/variables.lua
+++ nvim/lua/variables.lua
@@ -3,4 +3,5 @@ local g = vim.g
 g.rainbow_active = true
 g.mapleader = ' '
 g.completion_enable_snippet = 'UltiSnips'
+g.UltiSnipsExpandTrigger = '<c-e>'
 g.UltiSnipsSnippetDirectories = {"ultisnippets"}
