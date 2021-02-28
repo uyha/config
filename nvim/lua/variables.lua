6b493d566e82a70f0404e96ea9b93efe062fc189
Uy Ha <hchanuy@gmail.com>
2021-02-28 13:12:52 +0100

    Neovim: Set python3 path

--- nvim/lua/variables.lua
+++ nvim/lua/variables.lua
@@ -3,5 +3,6 @@ local g = vim.g
 g.rainbow_active = true
 g.mapleader = ' '
 g.completion_enable_snippet = 'UltiSnips'
+g.python3_host_prog = 'D:\\Local\\poetry\\virtualenvs\\poetry-Q0qgTqaX-py3.9\\Scripts\\python.exe'
 g.UltiSnipsExpandTrigger = '<c-e>'
 g.UltiSnipsSnippetDirectories = {"ultisnippets"}
