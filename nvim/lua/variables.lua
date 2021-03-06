14cc4c9dc296db2b2c7516cd0dc3611cfc94d70f
Uy Ha <hchanuy@gmail.com>
2021-03-06 22:29:06 +0100

    Neovim: format on save for rustfmt

--- nvim/lua/variables.lua
+++ nvim/lua/variables.lua
@@ -6,3 +6,4 @@ g.completion_enable_snippet = 'UltiSnips'
 g.python3_host_prog = 'D:\\Local\\poetry\\virtualenvs\\poetry-Q0qgTqaX-py3.9\\Scripts\\python.exe'
 g.UltiSnipsExpandTrigger = '<c-e>'
 g.UltiSnipsSnippetDirectories = {"ultisnippets"}
+g.rustfmt_autosave = 1
