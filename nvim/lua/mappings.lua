5e27fdb0246431c996a07cbe10dd207d470e7e1a
Uy Ha <hchanuy@gmail.com>
2021-04-10 12:48:02 +0200

    Map escape in terminal

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -2,7 +2,9 @@ local map = require 'utils'.map
 map('', '<leader>y', '"+y')
 map('', '<leader>d', '"+d')
 map('', '<leader>p', '"+p')
+
 map('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<S-Tab>"', {expr = true})
 map('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', {expr = true})
 map('i', '<C-k>', "<Plug>(completion_trigger)", {silent = true, noremap = false})
 
+map('t', '<Esc>', '<C-\\><C-n>') -- Make escape works in terminal emulator
