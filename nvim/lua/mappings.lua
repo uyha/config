f384b9e500c4aa2e3758d8ba5212e56f8438c803
Uy Ha <hchanuy@gmail.com>
2021-04-10 16:20:42 +0200

    Add mappings for resizing windows

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -4,10 +4,14 @@ map('', '<leader>d', '"+d')
 map('', '<leader>p', '"+p')
 
 map('', '<C-n>', ':NvimTreeToggle<CR>')
+map('', '<M-`>', ':15sp term://pwsh<CR>')
+map('', '<M-->', ':resize -5<CR>')
+map('', '<M-=>', ':resize +5<CR>')
+map('', '<M-,>', ':vertical resize -5<CR>')
+map('', '<M-.>', ':vertical resize +5<CR>')
 
 map('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<S-Tab>"', {expr = true})
 map('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', {expr = true})
 map('i', '<C-k>', "<Plug>(completion_trigger)", {silent = true, noremap = false})
 
 map('t', '<Esc>', '<C-\\><C-n>') -- Make escape works in terminal emulator
-
