ea6403c234340deca6cb2a8f92f624c76d39f648
Uy Ha <hchanuy@gmail.com>
2021-04-10 13:02:30 +0200

    Enable toggling NvimTree

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -3,8 +3,11 @@ map('', '<leader>y', '"+y')
 map('', '<leader>d', '"+d')
 map('', '<leader>p', '"+p')
 
+map('', '<C-n>', ':NvimTreeToggle<CR>')
+
 map('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<S-Tab>"', {expr = true})
 map('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', {expr = true})
 map('i', '<C-k>', "<Plug>(completion_trigger)", {silent = true, noremap = false})
 
 map('t', '<Esc>', '<C-\\><C-n>') -- Make escape works in terminal emulator
+
