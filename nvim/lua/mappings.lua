59a16a462a9f0b9f0b24af06d58871ea90eacc5a
Uy Ha <hchanuy@gmail.com>
2021-04-16 21:38:18 +0200

    Change mapping for openning terminal

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -4,7 +4,7 @@ map('', '<leader>p', '"+p')
 
 map('', '<C-n>', ':NvimTreeToggle<CR>')
 
-map('', '<lead>`', ':15sp term://pwsh<CR>')
+map('', '<leader>`', ':15sp term://pwsh<CR>')
 
 -- Windows movements
 map('', '<leader>-', ':resize -5<CR>')
