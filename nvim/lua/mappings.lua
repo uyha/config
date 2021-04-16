49725c5676880630ffbb3173a5c71fc4db8a077b
Uy Ha <hchanuy@gmail.com>
2021-04-16 13:03:39 +0200

    Update windows shortcuts

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -3,11 +3,19 @@ map('', '<leader>y', '"+y')
 map('', '<leader>p', '"+p')
 
 map('', '<C-n>', ':NvimTreeToggle<CR>')
+
 map('', '<M-`>', ':15sp term://pwsh<CR>')
-map('', '<M-->', ':resize -5<CR>')
-map('', '<M-=>', ':resize +5<CR>')
-map('', '<M-,>', ':vertical resize -5<CR>')
-map('', '<M-.>', ':vertical resize +5<CR>')
+
+-- Windows movements
+map('', '<leader>-', ':resize -5<CR>')
+map('', '<leader>=', ':resize +5<CR>')
+map('', '<leader>,', ':vertical resize -5<CR>')
+map('', '<leader>.', ':vertical resize +5<CR>')
+
+map('', '<leader>h', '<C-w>h')
+map('', '<leader>j', '<C-w>j')
+map('', '<leader>k', '<C-w>k')
+map('', '<leader>l', '<C-w>l')
 
 map('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<S-Tab>"', {expr = true})
 map('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', {expr = true})
