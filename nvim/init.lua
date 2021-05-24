7c4aa7cad9c8b20b4135a0e40a0ee91b5b1d1945
Uy Ha <hchanuy@gmail.com>
2021-05-24 14:52:11 +0200

    Move leadermap to beginning of init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,10 @@
 -- Adapted from https://github.com/ojroques/dotfiles/blob/master/nvim/init.lua
 
+local cmd = vim.cmd
+local g   = vim.g
+
+g.mapleader = ' '
+
 require 'lsp'
 require 'mappings'
 require 'options'
@@ -7,7 +12,6 @@ require 'plugins'
 require 'treesitter'
 require 'variables'
 
-local cmd = vim.cmd
 cmd [[lang en_US.utf8]]
 cmd [[colorscheme onedark]]
 cmd [[autocmd BufEnter * lua require'completion'.on_attach()]]
--- nvim/lua/variables.lua
+++ nvim/lua/variables.lua
@@ -1,7 +1,6 @@
 local g = vim.g
 
 g.rainbow_active = true
-g.mapleader = ' '
 g.completion_enable_snippet = 'UltiSnips'
 
 g.UltiSnipsExpandTrigger = '<c-e>'
