e220729a15c733eaafbdb1f0df1ba44b965b33af
Uy Ha <hchanuy@gmail.com>
2021-07-07 09:42:23 +0200

    Add .stylua.toml and rerun stylua

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,19 +3,19 @@ local g = vim.g
 
 g.mapleader = " "
 
-require("plugins")
-require("mappings")
-require("options")
-require("variables")
-require("setup")
-require("branch_mod")
+require "plugins"
+require "mappings"
+require "options"
+require "variables"
+require "setup"
+require "branch_mod"
 
-cmd([[lang en_US.utf8]])
-cmd([[colorscheme base16-onedark]])
+cmd [[lang en_US.utf8]]
+cmd [[colorscheme base16-onedark]]
 -- Automatically format file when it is saved
-cmd([[
+cmd [[
 augroup fmt
   autocmd!
   autocmd BufWritePre * try | undojoin | Neoformat | catch /^Vim\%((\a\+)\)\=:E790/ | finally | silent Neoformat | endtry
 augroup END
-]])
+]]
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,41 +1,41 @@
 local cmd = vim.cmd
 
-cmd([[packadd packer.nvim]])
-local packer = require("packer")
+cmd [[packadd packer.nvim]]
+local packer = require "packer"
 local use = packer.use
 
 packer.init()
 
-use({ "PProvost/vim-ps1" })
-use({ "RRethy/nvim-base16" })
-use({ "cespare/vim-toml" })
-use({
-	"folke/which-key.nvim",
-	config = function()
-		require("which-key").setup({})
-	end,
-})
-use({
-	"folke/todo-comments.nvim",
-	requires = "nvim-lua/plenary.nvim",
-	config = function()
-		require("todo-comments").setup({})
-	end,
-})
-use({ "hrsh7th/nvim-compe" })
-use({ "kyazdani42/nvim-tree.lua" })
-use({ "kyazdani42/nvim-web-devicons" })
-use({ "neovim/nvim-lspconfig" })
-use({ "nvim-telescope/telescope.nvim", requires = { { "nvim-lua/popup.nvim" }, { "nvim-lua/plenary.nvim" } } })
-use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-use({ "nvim-treesitter/playground" })
-use({ "p00f/nvim-ts-rainbow" })
-use({ "rust-lang/rust.vim" })
-use({ "sbdchd/neoformat" })
-use({ "sirver/UltiSnips" })
-use({ "tpope/vim-commentary" })
-use({ "tpope/vim-fugitive" })
-use({ "tpope/vim-surround" })
-use({ "vim-airline/vim-airline" })
-use({ "vim-airline/vim-airline-themes" })
-use({ "wbthomason/packer.nvim", opt = true })
+use { "PProvost/vim-ps1" }
+use { "RRethy/nvim-base16" }
+use { "cespare/vim-toml" }
+use {
+  "folke/which-key.nvim",
+  config = function()
+    require("which-key").setup {}
+  end,
+}
+use {
+  "folke/todo-comments.nvim",
+  requires = "nvim-lua/plenary.nvim",
+  config = function()
+    require("todo-comments").setup {}
+  end,
+}
+use { "hrsh7th/nvim-compe" }
+use { "kyazdani42/nvim-tree.lua" }
+use { "kyazdani42/nvim-web-devicons" }
+use { "neovim/nvim-lspconfig" }
+use { "nvim-telescope/telescope.nvim", requires = { { "nvim-lua/popup.nvim" }, { "nvim-lua/plenary.nvim" } } }
+use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
+use { "nvim-treesitter/playground" }
+use { "p00f/nvim-ts-rainbow" }
+use { "rust-lang/rust.vim" }
+use { "sbdchd/neoformat" }
+use { "sirver/UltiSnips" }
+use { "tpope/vim-commentary" }
+use { "tpope/vim-fugitive" }
+use { "tpope/vim-surround" }
+use { "vim-airline/vim-airline" }
+use { "vim-airline/vim-airline-themes" }
+use { "wbthomason/packer.nvim", opt = true }
