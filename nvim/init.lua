f356f41a6d92c364bc6d1a7c5604515a82e40bba
Uy Ha <hchanuy@gmail.com>
2021-10-10 14:51:57 -0700

    nvim: disable format on save

--- nvim/init.lua
+++ nvim/init.lua
@@ -13,10 +13,3 @@ require "statusline"
 
 cmd [[lang en_US.utf8]]
 cmd [[colorscheme base16-onedark]]
--- Automatically format file when it is saved
-cmd [[
-augroup fmt
-  autocmd!
-  autocmd BufWritePre * try | undojoin | Neoformat | catch /^Vim\%((\a\+)\)\=:E790/ | finally | silent Neoformat | endtry
-augroup END
-]]
