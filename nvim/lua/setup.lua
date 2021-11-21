dc25106dcf86a734e428f1401fe52cfd8bbb8949
Uy Ha <hchanuy@gmail.com>
2021-11-21 17:15:57 +0100

    Add svelte lsp

--- nvim/lua/setup.lua
+++ nvim/lua/setup.lua
@@ -2,7 +2,7 @@
 local lsp = require "lspconfig"
 local on_attach = require("utils").on_attach
 
-local servers = { "pyright", "cmake", "clangd", "rust_analyzer", "tsserver" }
+local servers = { "pyright", "cmake", "clangd", "rust_analyzer", "tsserver", "svelte"}
 for _, server in ipairs(servers) do
   lsp[server].setup {
     on_attach = on_attach,
