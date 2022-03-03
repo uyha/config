2a401040e6561ac6faee3cc20193edc01ca3be4b
Uy Ha <hchanuy@gmail.com>
2022-03-03 23:16:38 +0100

    Update bash config for fzf

--- bash/config.d/fzf.bash
+++ bash/config.d/fzf.bash
@@ -1,6 +1,10 @@
 config-fzf() {
-  local FZF_COMPLETION=$LOCAL_OPT/fzf/share/shell/completion.bash
-  local FZF_KEY_BINDINGS=$LOCAL_OPT/fzf/share/shell/key-bindings.bash
+  local FZF_HOME=$HOME/.fzf
+  local FZF_BIN=$FZF_HOME/bin
+  local FZF_COMPLETION=$FZF_HOME/shell/completion.bash
+  local FZF_KEY_BINDINGS=$FZF_HOME/shell/key-bindings.bash
+
+  [[ -d $FZF_BIN ]] && PATH+=:$FZF_BIN
   [[ -f $FZF_COMPLETION ]] && source $FZF_COMPLETION
   [[ -f $FZF_KEY_BINDINGS ]] && source $FZF_KEY_BINDINGS
 }
