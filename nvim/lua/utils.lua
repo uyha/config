12ef8aef586f9a4314f213f5778a8bf8330a2382
Uy Ha <hchanuy@gmail.com>
2022-03-04 22:25:30 +0100

    Update nvim-lsp `on_attach`

--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -13,61 +13,37 @@ utils.map = function(mode, lhs, rhs, opts)
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
-utils.on_attach = function(client, bufnr)
-  local map = function(...)
-    vim.api.nvim_buf_set_keymap(bufnr, ...)
-  end
-  local opt = function(...)
-    vim.api.nvim_buf_set_option(bufnr, ...)
-  end
+local opts = { noremap = true, silent = true }
+vim.api.nvim_set_keymap("n", "<space>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+vim.api.nvim_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
+vim.api.nvim_set_keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+vim.api.nvim_set_keymap("n", "<space>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
 
-  opt("omnifunc", "v:lua.vim.lsp.omnifunc")
+utils.on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
   -- Mappings.
-  local opts = { noremap = true, silent = true }
-  map("n", "<C-k>", [[<cmd>lua vim.lsp.buf.signature_help()<CR>]], opts)
-
-  map("n", "<leader>D", [[<cmd>lua vim.lsp.buf.type_definition()<CR>]], opts)
-  map("n", "<leader>ca", [[<cmd>lua vim.lsp.buf.code_action()<CR>]], opts)
-  map("n", "<leader>e", [[<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>]], opts)
-  map("n", "<leader>e", [[<cmd>lua vim.diagnostic.open_float()<CR>]], opts)
-  map("n", "<leader>q", [[<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>]], opts)
-  map("n", "<leader>rn", [[<cmd>lua vim.lsp.buf.rename()<CR>]], opts)
-  map("n", "<leader>wa", [[<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>]], opts)
-  map("n", "<leader>wl", [[<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>]], opts)
-  map("n", "<leader>wr", [[<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>]], opts)
-
-  map("n", "K", [[<Cmd>lua vim.lsp.buf.hover()<CR>]], opts)
-  map("n", "[d", [[<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>]], opts)
-  map("n", "]d", [[<cmd>lua vim.lsp.diagnostic.goto_next()<CR>]], opts)
-  map("n", "gD", [[<Cmd>lua vim.lsp.buf.declaration()<CR>]], opts)
-  map("n", "gd", [[<Cmd>lua vim.lsp.buf.definition()<CR>]], opts)
-  map("n", "gi", [[<cmd>lua vim.lsp.buf.implementation()<CR>]], opts)
-  map("n", "gr", [[<cmd>lua vim.lsp.buf.references()<CR>]], opts)
-
-  -- Set some keybinds conditional on server capabilities
-  if client.resolved_capabilities.document_formatting then
-    map("n", "<leader>f", [[<cmd>lua vim.lsp.buf.formatting()<CR>]], opts)
-  elseif client.resolved_capabilities.document_range_formatting then
-    map("n", "<leader>f", [[<cmd>lua vim.lsp.buf.range_formatting()<CR>]], opts)
-  end
-
-  -- Set autocommands conditional on server_capabilities
-  if client.resolved_capabilities.document_highlight then
-    vim.api.nvim_exec(
-      [[
-      hi LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
-      hi LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
-      hi LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
-      augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-    ]],
-      false
-    )
-  end
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(
+    bufnr,
+    "n",
+    "<space>wl",
+    "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
+    opts
+  )
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
 end
 
 return utils
