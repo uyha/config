cabffd3e72560942e7e083d678436a229aa0b969
Uy Ha <hchanuy@gmail.com>
2022-03-07 20:29:02 +0100

    Silent ssh-agent output

--- bash/config.d/ssh.bash
+++ bash/config.d/ssh.bash
@@ -1,4 +1,6 @@
 # shellcheck shell=bash
-eval "$(ssh-agent)"
-ssh-add "$HOME/.ssh/id_rsa"
-ssh-add "$HOME/.ssh/work"
+(
+  eval "$(ssh-agent)"
+  ssh-add "$HOME/.ssh/id_rsa"
+  ssh-add "$HOME/.ssh/work"
+) >/dev/null
