ef0db945302f039a172b3f44a49fabbb72b151f1
Uy Ha <hchanuy@gmail.com>
2022-05-08 19:57:29 +0200

    feat(nvim): add mappings and options for null-ls

--- nvcustom/plugins/null-ls.lua
+++ nvcustom/plugins/null-ls.lua
@@ -2,6 +2,7 @@ local null_ls = require "null-ls"
 local builtins = null_ls.builtins
 local formatting = builtins.formatting
 local diagnostics = builtins.diagnostics
+local map = require("core.utils").map
 
 return {
    setup = function()
@@ -12,11 +13,23 @@ return {
             formatting.deno_fmt,
 
             formatting.stylua,
-            diagnostics.luacheck.with { extra_args = { "--global vim" } },
+            diagnostics.luacheck.with { extra_args = { "--globals vim" } },
 
-            formatting.shfmt,
+            formatting.shfmt.with {
+               extra_args = {"-i", "2"},
+            },
             diagnostics.shellcheck.with { diagnostics_format = "#{m} [#{c}]" },
          },
+         on_attach = function(client)
+            if client.resolved_capabilities.document_formatting then
+               map("n", "<leader>fm", function()
+                  vim.lsp.buf.formatting()
+               end)
+            end
+            map("n", "<leader>e", function()
+               vim.diagnostic.open_float()
+            end)
+         end,
       }
    end,
 }
