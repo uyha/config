8641e7eeda6ca95cdfff424fc2c75eb9dd9fb163
Uy Ha <hchanuy@gmail.com>
2022-05-10 16:20:36 +0200

    feat(nvim): configure relativenumber

--- nvcustom/init.lua
+++ nvcustom/init.lua
@@ -1,2 +1,19 @@
-vim.cmd([[set foldmethod=expr]])
-vim.cmd([[set foldexpr=nvim_treesitter#foldexpr()]])
+local autocmd = vim.api.nvim_create_autocmd
+local cmd = vim.cmd
+
+cmd([[set foldmethod=expr]])
+cmd([[set foldexpr=nvim_treesitter#foldexpr()]])
+
+vim.opt.relativenumber = true
+
+autocmd("InsertEnter", {
+  callback = function()
+    vim.opt.relativenumber = false
+  end
+})
+
+autocmd("InsertLeave", {
+  callback = function()
+    vim.opt.relativenumber = true
+  end
+})
