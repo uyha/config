2bffdbf6bbf510f4e8a2a084decd9db28b97221c
Uy Ha <hchanuy@gmail.com>
2022-05-12 01:29:33 +0200

    feat(nvim): specify path to snippets

--- nvcustom/plugins/luasnip/init.lua
+++ nvcustom/plugins/luasnip/init.lua
@@ -2,7 +2,9 @@ local M = {}
 
 M.setup = function()
    require("plugins.configs.others").luasnip()
-   require("luasnip.loaders.from_snipmate").load()
+   require("luasnip.loaders.from_snipmate").load {
+      paths = { "~/.config/nvcustom/snippets" },
+   }
 end
 
 return M
