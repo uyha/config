4d06d085ce09371c4915ac84574c6e11b897210f
Uy Ha <hchanuy@gmail.com>
2022-05-12 12:55:07 +0200

    feat(nvim): enable number line

--- nvcustom/init.lua
+++ nvcustom/init.lua
@@ -1,19 +1,20 @@
 local autocmd = vim.api.nvim_create_autocmd
 local cmd = vim.cmd
 
-cmd([[set foldmethod=expr]])
-cmd([[set foldexpr=nvim_treesitter#foldexpr()]])
+cmd [[set foldmethod=expr]]
+cmd [[set foldexpr=nvim_treesitter#foldexpr()]]
 
+vim.opt.number = true
 vim.opt.relativenumber = true
 
 autocmd("InsertEnter", {
-  callback = function()
-    vim.opt.relativenumber = false
-  end
+   callback = function()
+      vim.opt.relativenumber = false
+   end,
 })
 
 autocmd("InsertLeave", {
-  callback = function()
-    vim.opt.relativenumber = true
-  end
+   callback = function()
+      vim.opt.relativenumber = true
+   end,
 })
