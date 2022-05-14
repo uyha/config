09e009e84f1235f83c15eb5e5a57bb5114c8a59c
Uy Ha <hchanuy@gmail.com>
2022-05-14 03:13:41 +0200

    feat(nvim): add function for switching cases

--- nvcustom/plugins/luasnip.lua
+++ nvcustom/plugins/luasnip.lua
@@ -1,5 +1,31 @@
 local M = {}
 
+local function snake_case(str)
+   str = str:gsub("::", "/")
+   str = str:gsub("(%u+)(%u%l)", "%1_%2")
+   str = str:gsub("(%u+)(%u%l)", "%1_%2")
+   str = str:gsub("(%l)(%u)", "%1_%2")
+   str = str:gsub("(%d)(%u)", "%1_%2")
+   str = str:gsub("[.-]", "_")
+   return str:lower()
+end
+
+local function camel_case(str)
+   str = str:gsub("-", "_")
+
+   local _start, _end = str:find "^_+"
+   _start = _start or 0
+   _end = _end or 0
+
+   str = str:sub(_start, _end) .. str:sub(_end + 1):gsub("_(%l)", string.upper)
+   return str
+end
+
+local function mixed_case(str)
+   str = camel_case(str):gsub("^(_*%l)", string.upper)
+   return str
+end
+
 local function add_snippets()
    local ls = require "luasnip"
    local s = ls.snippet
