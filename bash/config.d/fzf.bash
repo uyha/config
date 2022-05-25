1ed806a2e27c0f31f956b885d93123474d8cfc3e
Uy Ha <hchanuy@gmail.com>
2022-05-25 22:48:36 +0200

    feat: update fzf setup script

--- bash/config.d/fzf.bash
+++ bash/config.d/fzf.bash
@@ -1,12 +1,8 @@
+# shellcheck shell=bash
 config-fzf() {
-  local FZF_HOME=$HOME/.fzf
-  local FZF_BIN=$FZF_HOME/bin
-  local FZF_COMPLETION=$FZF_HOME/shell/completion.bash
-  local FZF_KEY_BINDINGS=$FZF_HOME/shell/key-bindings.bash
-
-  [[ -d $FZF_BIN && ! "$PATH" =~ "$FZF_BIN" ]] && PATH+=:$FZF_BIN
-  [[ -f $FZF_COMPLETION ]] && source $FZF_COMPLETION
-  [[ -f $FZF_KEY_BINDINGS ]] && source $FZF_KEY_BINDINGS
+  if [[ -f /usr/share/doc/fzf/examples/key-bindings.bash ]]; then
+    source /usr/share/doc/fzf/examples/key-bindings.bash
+  fi
 }
 
 config-fzf
