d225bd9142fc17ea6018b7d465cf89966522dfb3
Uy Ha <hchanuy@gmail.com>
2022-05-29 19:47:21 +0200

    feat(bash): rename key-bindings to bindings

--- bash/config.d/bindings.bash
+++ bash/config.d/bindings.bash
@@ -1,6 +1,6 @@
 # shellcheck shell=bash
 # shellcheck source=/dev/null
 
-for completion_file in /usr/local/key-bindings/*.bash; do
+for completion_file in /usr/local/share/bindings/*.bash; do
   source "$completion_file"
 done
