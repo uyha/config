f279c8e470b8d58794ebead164e9fe297014ea56
Uy Ha <hchanuy@gmail.com>
2022-05-31 19:00:40 +0200

    feat: include completions and bindings in /usr/local/share

--- bash/config.d/bindings.bash
+++ bash/config.d/bindings.bash
@@ -1,6 +1,6 @@
 # shellcheck shell=bash
 # shellcheck source=/dev/null
 
-for completion_file in /usr/local/share/bindings/*.bash; do
+for completion_file in /usr/local/share/bash-binding/bindings/*.bash; do
   source "$completion_file"
 done
--- bash/config.d/completions.bash
+++ bash/config.d/completions.bash
@@ -1,6 +1,6 @@
 # shellcheck shell=bash
 # shellcheck source=/dev/null
 
-for completion_file in /usr/local/completions/*.bash; do
+for completion_file in /usr/local/share/bash-completion/completions/*.bash; do
   source "$completion_file"
 done
