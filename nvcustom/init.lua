7b1e0d380b4fd836d1ee72048d395fd32ede468f
Uy Ha <hchanuy@gmail.com>
2022-06-05 11:46:11 +0200

    feat(nvim): remove autocmd for relativenumber

--- nvcustom/init.lua
+++ nvcustom/init.lua
@@ -1,4 +1,3 @@
-local autocmd = vim.api.nvim_create_autocmd
 local cmd = vim.cmd
 
 cmd [[set foldmethod=expr]]
@@ -6,15 +5,3 @@ cmd [[set foldexpr=nvim_treesitter#foldexpr()]]
 
 vim.opt.number = true
 vim.opt.relativenumber = true
-
-autocmd("InsertEnter", {
-   callback = function()
-      vim.opt.relativenumber = false
-   end,
-})
-
-autocmd("InsertLeave", {
-   callback = function()
-      vim.opt.relativenumber = true
-   end,
-})
