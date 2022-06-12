e699b382384d627abcb35bb149e366cca7aade4d
Uy Ha <hchanuy@gmail.com>
2022-06-12 22:16:27 +0200

    feat(bash): include bash-completion if available

--- bash/config.d/completions.bash
+++ bash/config.d/completions.bash
@@ -1,6 +1,12 @@
 # shellcheck shell=bash
 # shellcheck source=/dev/null
 
-for completion_file in /usr/local/share/bash-completion/completions/*.bash; do
-  source "$completion_file"
-done
+if [[ "$-" == *i* ]]; then
+  for completion_file in /usr/local/share/bash-completion/completions/*.bash; do
+    source "$completion_file"
+  done
+
+  if [[ -f /usr/share/bash-completion/bash_completion ]]; then
+    source /usr/share/bash-completion/bash_completion
+  fi
+fi
