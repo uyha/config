69c9323db9330e6892e1ebc181efea6f8595b2aa
Uy Ha <hchanuy@gmail.com>
2022-06-14 11:20:42 +0200

    feat: use nvim as mergetool for git

--- nvcustom/plugins/init.lua
+++ nvcustom/plugins/init.lua
@@ -6,6 +6,7 @@ return {
       end,
    },
    ["tpope/vim-abolish"] = {},
+   ["tpope/vim-fugitive"] = {},
    ["tpope/vim-surround"] = {},
    ["L3MON4D3/LuaSnip"] = {
       wants = "friendly-snippets",
