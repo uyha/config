67647d199f13d0a25c3917d4cee09f2e60b71556
Uy Ha <hchanuy@gmail.com>
2022-08-01 14:26:14 +0200

    fix(ble.sh): extract the directory name from fzf execution path

--- blesh/init.sh
+++ blesh/init.sh
@@ -1,9 +1,11 @@
 # shellcheck shell=bash
 
 # fzf
-_ble_contrib_fzf_base="$(which fzf)"
-ble-import -d contrib/fzf-completion
-ble-import -d contrib/fzf-key-bindings
+if command -v fzf &>/dev/null; then
+  _ble_contrib_fzf_base="$(realpath $(dirname $(which fzf))/..)"
+  ble-import -d contrib/fzf-completion
+  ble-import -d contrib/fzf-key-bindings
+fi
 
 # History
 bleopt history_share=yes
