36df6ff040ebd7076fa0e5425a6b8f9336e00bc1
Uy Ha <hchanuy@gmail.com>
2022-08-03 10:05:52 +0200

    feat(nvim): enable git integration for nvim-tree and format chadrc.lua

--- nvcustom/chadrc.lua
+++ nvcustom/chadrc.lua
@@ -3,44 +3,47 @@ local M = {}
 M.ui = { theme = "onedark" }
 
 M.plugins = {
-   user = {
-      ["jose-elias-alvarez/null-ls.nvim"] = {
-         after = "nvim-lspconfig",
-         config = function()
-            require("custom.null-ls").setup()
-         end,
+  user = {
+    ["jose-elias-alvarez/null-ls.nvim"] = {
+      after = "nvim-lspconfig",
+      config = function()
+        require("custom.null-ls").setup()
+      end,
+    },
+    ["tpope/vim-abolish"] = {},
+    ["tpope/vim-fugitive"] = {},
+    ["tpope/vim-surround"] = {},
+    ["L3MON4D3/LuaSnip"] = {
+      after = "nvim-cmp",
+      config = function()
+        require("custom.luasnip").setup()
+      end,
+    },
+    ["hrsh7th/nvim-cmp"] = {
+      config = function()
+        require "plugins.configs.cmp"
+      end,
+    },
+    ["wgwoods/vim-systemd-syntax"] = {},
+  },
+  options = {
+    lspconfig = {
+      setup_lspconf = "custom.lspconfig",
+    },
+  },
+  override = {
+    ["kyazdani42/nvim-tree.lua"] = {
+      actions = {
+        open_file = {
+          resize_window = false,
+        },
       },
-      ["tpope/vim-abolish"] = {},
-      ["tpope/vim-fugitive"] = {},
-      ["tpope/vim-surround"] = {},
-      ["L3MON4D3/LuaSnip"] = {
-         after = "nvim-cmp",
-         config = function()
-            require("custom.luasnip").setup()
-         end,
+      git = {
+        enable = true,
       },
-      ["hrsh7th/nvim-cmp"] = {
-         config = function()
-            require "plugins.configs.cmp"
-         end,
-      },
-      ["wgwoods/vim-systemd-syntax"] = {},
-   },
-   options = {
-      lspconfig = {
-         setup_lspconf = "custom.lspconfig",
-      },
-   },
-   override = {
-      ["kyazdani42/nvim-tree.lua"] = {
-         actions = {
-            open_file = {
-               resize_window = false,
-            },
-         },
-      },
-   },
-   remove = {},
+    },
+  },
+  remove = {},
 }
 
 M.mappings = require "custom.mappings"
