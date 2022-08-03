d248bc7b15cc14a67af339d5caad18e355f45197
Uy Ha <hchanuy@gmail.com>
2022-08-03 13:59:01 +0200

    feat(nvim): switch to using prettier for formatting and format file

--- nvcustom/null-ls.lua
+++ nvcustom/null-ls.lua
@@ -4,37 +4,36 @@ local formatting = builtins.formatting
 local diagnostics = builtins.diagnostics
 
 return {
-   setup = function()
-      null_ls.setup {
-         debug = true,
-         sources = {
-            -- Web stuff
-            formatting.prettierd.with { filetypes = { "html", "markdown", "css" } },
-            formatting.deno_fmt,
+  setup = function()
+    null_ls.setup {
+      debug = true,
+      sources = {
+        -- Web stuff
+        formatting.prettier,
 
-            -- Lua
-            formatting.stylua,
-            diagnostics.luacheck.with { extra_args = { "--globals", "vim" } },
+        -- Lua
+        formatting.stylua,
+        diagnostics.luacheck.with { extra_args = { "--globals", "vim" } },
 
-            -- Shell
-            formatting.shfmt.with { extra_args = { "-i", "2" } },
+        -- Shell
+        formatting.shfmt.with { extra_args = { "-i", "2" } },
 
-            -- C/C++
-            formatting.clang_format,
+        -- C/C++
+        formatting.clang_format,
 
-            -- Python
-            formatting.black,
+        -- Python
+        formatting.black,
 
-            -- Perl
-            formatting.perltidy,
-         },
-         on_attach = function(client)
-            if client.resolved_capabilities.document_formatting then
-               vim.keymap.set("n", "<leader>fm", function()
-                  vim.lsp.buf.formatting()
-               end)
-            end
-         end,
-      }
-   end,
+        -- Perl
+        formatting.perltidy,
+      },
+      on_attach = function(client)
+        if client.resolved_capabilities.document_formatting then
+          vim.keymap.set("n", "<leader>fm", function()
+            vim.lsp.buf.formatting()
+          end)
+        end
+      end,
+    }
+  end,
 }
