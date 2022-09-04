cd153124decd0f6da45f4a01011ea13de2fd502a
Uy Ha <hchanuy@gmail.com>
2022-09-04 19:45:59 +0200

    feat(nvim): add cssls

--- nvcustom/lspconfig.lua
+++ nvcustom/lspconfig.lua
@@ -2,7 +2,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "pyright", "bashls", "svelte", "tsserver", "rust_analyzer" }
+local servers = { "pyright", "bashls", "svelte", "tsserver", "rust_analyzer", "cssls" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
