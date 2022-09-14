d8f9885c03af4aa8e4889e34adbac255be406324
Uy Ha <hchanuy@gmail.com>
2022-09-14 16:21:09 +0200

    refactor(nvim): format file

--- nvcustom/mappings.lua
+++ nvcustom/mappings.lua
@@ -1,74 +1,74 @@
 local M = {}
 
 local function termcodes(str)
-   return vim.api.nvim_replace_termcodes(str, true, true, true)
+  return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
 
 M.general = {
-   n = {
-      ["<leader>e"] = {
-         function()
-            vim.diagnostic.open_float()
-         end,
-         " open diagnostic on a floating window",
-      },
-      ["<leader>."] = {
-         function()
-            vim.cmd "vertical resize +10"
-         end,
-         "Narrow the current window",
-      },
-      ["<leader>,"] = {
-         function()
-            vim.cmd "vertical resize -10"
-         end,
-         "Widen the current window",
-      },
-      ["<leader>="] = {
-         function()
-            vim.cmd "resize +10"
-         end,
-         "Increase the height of current window",
-      },
-      ["<leader>-"] = {
-         function()
-            vim.cmd "resize -10"
-         end,
-         "Decrease the height of current window",
-      },
-   },
-   t = {
-      ["<C-x>"] = { termcodes "<C-\\><C-N>", "   escape terminal mode" },
-   },
+  n = {
+    ["<leader>e"] = {
+      function()
+        vim.diagnostic.open_float()
+      end,
+      " open diagnostic on a floating window",
+    },
+    ["<leader>."] = {
+      function()
+        vim.cmd "vertical resize +10"
+      end,
+      "Narrow the current window",
+    },
+    ["<leader>,"] = {
+      function()
+        vim.cmd "vertical resize -10"
+      end,
+      "Widen the current window",
+    },
+    ["<leader>="] = {
+      function()
+        vim.cmd "resize +10"
+      end,
+      "Increase the height of current window",
+    },
+    ["<leader>-"] = {
+      function()
+        vim.cmd "resize -10"
+      end,
+      "Decrease the height of current window",
+    },
+  },
+  t = {
+    ["<C-x>"] = { termcodes "<C-\\><C-N>", "   escape terminal mode" },
+  },
 }
 
 M.nvterm = {
-   t = {
-      ["<A-t>"] = {
-         function()
-            require("nvterm.terminal").toggle "horizontal"
-         end,
-         "   toggle horizontal term",
-      },
-   },
-   n = {
-      ["<A-t>"] = {
-         function()
-            require("nvterm.terminal").toggle "horizontal"
-         end,
-         "   toggle horizontal term",
-      },
-   },
+  t = {
+    ["<A-t>"] = {
+      function()
+        require("nvterm.terminal").toggle "horizontal"
+      end,
+      "   toggle horizontal term",
+    },
+  },
+  n = {
+    ["<A-t>"] = {
+      function()
+        require("nvterm.terminal").toggle "horizontal"
+      end,
+      "   toggle horizontal term",
+    },
+  },
 }
 
 M.disabled = {
-   n = {
-      ["<A-h>"] = "",
-   },
-   t = {
-      ["jk"] = "",
-      ["<A-h>"] = "",
-   },
+  n = {
+    ["<A-h>"] = "",
+  },
+  t = {
+    ["jk"] = "",
+    ["<A-h>"] = "",
+  },
 }
 
 return M
