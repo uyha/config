3f58e0e73ee8b2cfcefe62632a25b8c10c34f1e1
Uy Ha <hchanuy@gmail.com>
2022-09-14 16:14:51 +0200

    chore(nvim): update NvChad and adapt config

--- nvcustom/chadrc.lua
+++ nvcustom/chadrc.lua
@@ -3,41 +3,39 @@ local M = {}
 M.ui = { theme = "onedark" }
 
 M.plugins = {
-  user = {
-    ["jose-elias-alvarez/null-ls.nvim"] = {
-      after = "nvim-lspconfig",
-      config = function()
-        require("custom.null-ls").setup()
-      end,
-    },
-    ["tpope/vim-abolish"] = {},
-    ["tpope/vim-fugitive"] = {},
-    ["kylechui/nvim-surround"] = {
-      config = function()
-        require("nvim-surround").setup()
-      end,
-    },
-    ["L3MON4D3/LuaSnip"] = {
-      after = "nvim-cmp",
-      config = function()
-        require("custom.luasnip").setup()
-      end,
-    },
-    ["hrsh7th/nvim-cmp"] = {
-      config = function()
-        require "plugins.configs.cmp"
-      end,
-    },
-    ["wgwoods/vim-systemd-syntax"] = {},
-    ["neovim/nvim-lspconfig"] = {
-      config = function()
-        require "plugins.configs.lspconfig"
-        require "custom.lspconfig"
-      end,
-    },
+  ["jose-elias-alvarez/null-ls.nvim"] = {
+    after = "nvim-lspconfig",
+    config = function()
+      require("custom.null-ls").setup()
+    end,
   },
-  override = {
-    ["kyazdani42/nvim-tree.lua"] = {
+  ["tpope/vim-abolish"] = {},
+  ["tpope/vim-fugitive"] = {},
+  ["kylechui/nvim-surround"] = {
+    config = function()
+      require("nvim-surround").setup()
+    end,
+  },
+  ["L3MON4D3/LuaSnip"] = {
+    after = "nvim-cmp",
+    config = function()
+      require("custom.luasnip").setup()
+    end,
+  },
+  ["hrsh7th/nvim-cmp"] = {
+    config = function()
+      require "plugins.configs.cmp"
+    end,
+  },
+  ["wgwoods/vim-systemd-syntax"] = {},
+  ["neovim/nvim-lspconfig"] = {
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.lspconfig"
+    end,
+  },
+  ["kyazdani42/nvim-tree.lua"] = {
+    override_options = {
       actions = {
         open_file = {
           resize_window = false,
@@ -48,7 +46,6 @@ M.plugins = {
       },
     },
   },
-  remove = {},
 }
 
 M.mappings = require "custom.mappings"
