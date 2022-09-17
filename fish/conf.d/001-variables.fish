0fbfb4d7cc6a90ed96771086e8301d3754691f93
Uy Ha <hchanuy@gmail.com>
2022-09-17 10:25:37 +0200

    feat(fish): add all bin directories in $LOCAL

--- fish/conf.d/001-variables.fish
+++ fish/conf.d/001-variables.fish
@@ -12,7 +12,9 @@ set -gx XDG_DATA_HOME $LOCAL/share
 set -gx XDG_STATE_HOME $LOCAL/state
 set -gx XDG_CACHE_HOME $HOME/.cache
 
-set -gx PATH $PATH $LOCAL/bin $LOCAL/opt/cargo/bin
+for dir in $LOCAL/bin $LOCAL/opt/*/bin
+  set -gx PATH $PATH $dir
+end
 
 if command --query nvim;
   set -gx MANPAGER 'nvim +Man!'
