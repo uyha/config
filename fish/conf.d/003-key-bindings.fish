703338a9594c9211eff177d83e32b6553b263df7
Uy Ha <hchanuy@gmail.com>
2022-09-17 19:44:04 +0200

    fix(fish): correct fzf bindings path

--- fish/conf.d/003-key-bindings.fish
+++ fish/conf.d/003-key-bindings.fish
@@ -2,7 +2,7 @@ if ! status is-interactive;
   exit 0
 end
 
-if [ -f /usr/local/share/fish-binding/fzf.fish ];
-  source /usr/local/share/fish-binding/fzf.fish
+if [ -f /usr/local/share/fish-bindings/fzf.fish ];
+  source /usr/local/share/fish-bindings/fzf.fish
   fzf_key_bindings
 end
