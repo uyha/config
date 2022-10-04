546a1eceb0cfcdf6b57ec9814f45b37bc0b72613
Uy Ha <hchanuy@gmail.com>
2022-10-04 12:53:10 +0200

    fix(fish): check before sourcing completions

--- fish/conf.d/004-completions.fish
+++ fish/conf.d/004-completions.fish
@@ -2,8 +2,13 @@ if ! status is-interactive;
   exit 0
 end
 
-if [ -d /home/linuxbrew/.linuxbrew/share/fish/vendor_completions.d ]
+set vendor_completions "$HOMEBREW_PREFIX/share/fish/vendor_completions.d"
+
+if [ -d "$vendor_completions" ]
   for f in brew dasel exa fd git-cliff rg starship tldr;
-    source "/home/linuxbrew/.linuxbrew/share/fish/vendor_completions.d/$f.fish"
+    set comp_file "$vendor_completions/$f.fish"
+    [ -f "$comp_file" ] && source "$comp_file"
   end
 end
+
+set --erase vendor_completions comp_file
