7dd510c0e5375e88dc8c93d00633bb489fe2b7eb
Uy Ha <hchanuy@gmail.com>
2022-10-07 20:07:29 +0200

    feat(fish): include rustup default toolchain

--- fish/conf.d/001-variables.fish
+++ fish/conf.d/001-variables.fish
@@ -12,13 +12,12 @@ set -gx XDG_DATA_HOME $LOCAL/share
 set -gx XDG_STATE_HOME $LOCAL/state
 set -gx XDG_CACHE_HOME $HOME/.cache
 
-for dir in $LOCAL/bin $LOCAL/opt/*/bin
-  set -gx PATH $PATH $dir
+for dir in $LOCAL/bin $LOCAL/opt/*/bin; set -gx PATH $PATH $dir; end
+if [ -d "$RUSTUP_HOME/toolchains/stable-x86_64-unknown-linux-gnu/bin" ];
+  set -gx PATH "$RUSTUP_HOME/toolchains/stable-x86_64-unknown-linux-gnu/bin" $PATH
 end
 
-if command --query nvim;
-  set -gx MANPAGER 'nvim +Man!'
-end
+if command --query nvim; set -gx MANPAGER 'nvim +Man!'; end
 
 if [ -f /home/linuxbrew/.linuxbrew/bin/brew ];
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
