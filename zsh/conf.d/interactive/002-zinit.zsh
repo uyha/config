74e0f5a85571a2cfce2aba6ef85dfe8273ed506b
Uy Ha <hchanuy@gmail.com>
2022-10-17 10:03:47 +0200

    feat(zsh): use zinit's turbo in decrease startup time

--- zsh/conf.d/interactive/002-zinit.zsh
+++ zsh/conf.d/interactive/002-zinit.zsh
@@ -5,7 +5,10 @@ fi
 export ZINIT_HOME="$HOMEBREW_PREFIX/opt/zinit"
 source "$ZINIT_HOME/zinit.zsh"
 
+zinit ice wait lucid
 zinit load "zsh-users/zsh-autosuggestions"
+
+zinit ice wait lucid
 zinit load "zsh-users/zsh-syntax-highlighting"
 
 zinit for atload"zicompinit; zicdreplay" blockf lucid wait "zsh-users/zsh-completions"
