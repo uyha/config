a3c54075649c078baa0c23ad69d300eb717cd7b7
Uy Ha <hchanuy@gmail.com>
2022-10-17 12:57:34 +0200

    feat(zsh): enable share history from different terminals

--- zsh/conf.d/interactive/004-options.zsh
+++ zsh/conf.d/interactive/004-options.zsh
@@ -1,2 +1,4 @@
 zstyle ':completion:*' menu yes select
 zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*' # Allow case insensitive matching
+
+setopt share_history
