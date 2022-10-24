e3c52dda058c9136abcec77a90852932cfb9d300
Uy Ha <hchanuy@gmail.com>
2022-10-25 00:20:48 +0200

    define 2 terminals: lazygit and main

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,46 +1,40 @@
 local M = {}
 
-local lazygit
+local termcodes = function(sequence) return vim.api.nvim_replace_termcodes(sequence, true, true, true) end
 
-local termcodes = function(sequence)
-  return vim.api.nvim_replace_termcodes(sequence, true, true, true)
-end
-
-local termesc = function(sequence)
-  return termcodes("<C-\\><C-N>" .. (sequence or ""))
-end
-
-local lazygit_terminal = function()
-  return require("toggleterm.terminal").Terminal:new {
-    cmd = "lazygit",
-    direction = "float",
-    hidden = true,
-  }
-end
-
-local toggle_lazygit = function()
-  lazygit = lazygit or lazygit_terminal()
-  lazygit:toggle()
-end
+local termesc = function(sequence) return termcodes("<C-\\><C-N>" .. (sequence or "")) end
 
 local on_open = function(term)
-  local config = { buffer = true }
+  local config = { buffer = true , silent = true}
 
   vim.keymap.set("t", "<Esc>", termesc(), config)
-  vim.keymap.set("t", "<M-t>", termesc ":ToggleTerm<CR>", config)
+  vim.keymap.set("t", "<M-t>", function() term:toggle() end, config)
   vim.keymap.set("t", "<C-h>", termesc "<C-w>h", config)
   vim.keymap.set("t", "<C-j>", termesc "<C-w>j", config)
   vim.keymap.set("t", "<C-k>", termesc "<C-w>k", config)
   vim.keymap.set("t", "<C-l>", termesc "<C-w>l", config)
 end
 
+local Terminal = require("toggleterm.terminal").Terminal
+
+local main = Terminal:new {
+  hidden = false,
+  on_open = on_open,
+}
+
+local lazygit = Terminal:new {
+  cmd = "lazygit",
+  direction = "float",
+  hidden = true,
+}
+
 M.setup = function()
   require("toggleterm").setup {
     on_open = on_open,
   }
 
-  vim.keymap.set("n", "<M-t>", ":ToggleTerm<CR>", { silent = true })
-  vim.keymap.set("n", "<leader>lzg", toggle_lazygit)
+  vim.keymap.set("n", "<M-t>", function() main:toggle() end)
+  vim.keymap.set("n", "<leader>lzg", function() lazygit:toggle() end)
 end
 
 return M
