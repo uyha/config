d88b13ac1c991518f74fdedac21207f92962d4d0
Uy Ha <hchanuy@gmail.com>
2022-10-27 00:48:47 +0200

    disable default plugins

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -10,3 +10,37 @@ vim.o.foldmethod = "expr"
 vim.o.foldexpr = "nvim_treesitter#foldexpr()"
 
 vim.g.mapleader = " "
+
+-- Disable default plugins
+local default_plugins = {
+  "2html_plugin",
+  "getscript",
+  "getscriptPlugin",
+  "gzip",
+  "logipat",
+  "netrw",
+  "netrwPlugin",
+  "netrwSettings",
+  "netrwFileHandlers",
+  "matchit",
+  "tar",
+  "tarPlugin",
+  "rrhelper",
+  "spellfile_plugin",
+  "vimball",
+  "vimballPlugin",
+  "zip",
+  "zipPlugin",
+  "tutor",
+  "rplugin",
+  "syntax",
+  "synmenu",
+  "optwin",
+  "compiler",
+  "bugreport",
+  "ftplugin",
+}
+
+for _, plug in pairs(default_plugins) do
+  vim.g["loaded_" .. plug] = true
+end
