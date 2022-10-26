92dd017784cb8665089a59efe041739b42c1bd5a
Uy Ha <hchanuy@gmail.com>
2022-10-26 08:25:14 +0200

    expand tab, set shiftwidth to 2, to treesitter for folding

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -2,8 +2,11 @@ vim.o.termguicolors = true
 vim.o.splitright = true --Vertical split on the right
 vim.o.number = true
 vim.o.relativenumber = true
+vim.o.expandtab = true
+vim.o.shiftwidth = 2
+
+-- Use treesitter for folding
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "nvim_treesitter#foldexpr()"
 
 vim.g.mapleader = " "
-
-vim.wo.foldmethod = "expr"
-vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
