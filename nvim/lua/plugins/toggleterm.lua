db0f695e1363855d7a49e3ac5a55ad3a679a9c20
Uy Ha <hchanuy@gmail.com>
2022-10-26 08:26:49 +0200

    refactor toggleterm config

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -4,28 +4,28 @@ local termcodes = function(sequence) return vim.api.nvim_replace_termcodes(seque
 
 local termesc = function(sequence) return termcodes("<C-\\><C-N>" .. (sequence or "")) end
 
-local on_open = function(term)
-  local config = { buffer = true, silent = true }
-
-  vim.keymap.set("t", "<Esc>", termesc(), config)
-  vim.keymap.set("t", "<M-t>", function() term:toggle() end, config)
-  vim.keymap.set("t", "<C-h>", termesc "<C-w>h", config)
-  vim.keymap.set("t", "<C-j>", termesc "<C-w>j", config)
-  vim.keymap.set("t", "<C-k>", termesc "<C-w>k", config)
-  vim.keymap.set("t", "<C-l>", termesc "<C-w>l", config)
-end
-
 local Terminal = require("toggleterm.terminal").Terminal
 
 local main = Terminal:new {
   hidden = false,
-  on_open = on_open,
+  on_open = function(term)
+    local config = { buffer = term.bufnr, silent = true }
+
+    vim.keymap.set({ "t", "n" }, "<M-t>", function() term:close() end, config)
+
+    vim.keymap.set("t", "<Esc>", termesc(), config)
+    vim.keymap.set("t", "<C-h>", termesc "<C-w>h", config)
+    vim.keymap.set("t", "<C-j>", termesc "<C-w>j", config)
+    vim.keymap.set("t", "<C-k>", termesc "<C-w>k", config)
+    vim.keymap.set("t", "<C-l>", termesc "<C-w>l", config)
+  end,
 }
 
 local lazygit = Terminal:new {
   cmd = "lazygit",
   direction = "float",
   hidden = true,
+  on_close = function() vim.cmd "NvimTreeRefresh" end,
 }
 
 M.setup = function()
@@ -33,8 +33,8 @@ M.setup = function()
     on_open = on_open,
   }
 
-  vim.keymap.set("n", "<M-t>", function() main:toggle() end)
-  vim.keymap.set("n", "<leader>lzg", function() lazygit:toggle() end)
+  vim.keymap.set("n", "<M-t>", function() main:open() end)
+  vim.keymap.set("n", "<leader>lzg", function() lazygit:open() end)
 end
 
 return M
