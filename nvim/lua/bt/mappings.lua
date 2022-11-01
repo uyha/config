556bf05602ed9868f15cca0a84503728a332f905
Uy Ha <hchanuy@gmail.com>
2022-11-01 23:40:06 +0100

    add descriptions for keymaps and remap fold keymaps to invoke IndentBlanklineRefresh

--- nvim/lua/bt/mappings.lua
+++ nvim/lua/bt/mappings.lua
@@ -12,32 +12,27 @@ vim.keymap.set("n", "<C-n>", function() vim.cmd [[NvimTreeToggle]] end)
 vim.keymap.set("n", "<C-f>", function() vim.cmd [[NvimTreeFindFile]] end)
 
 -- LSP
-vim.keymap.set("n", "<leader>e", function() vim.diagnostic.open_float() end, { desc = "Open diagnostic window" })
 vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, { desc = "Go to the previous diagnostic" })
 vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, { desc = "Go to the next diagnostic" })
+vim.keymap.set("n", "<leader>e", function() vim.diagnostic.open_float() end, { desc = "Open diagnostic window" })
 vim.keymap.set(
   "n",
   "<leader>q",
   function() vim.diagnostic.setloclist() end,
-  { desc = "Add diagnostic to the location list" }
-)
-
--- Noice
-vim.keymap.set("n", "<leader>nh", function() require("noice").cmd "history" end, { desc = "Show history" })
-vim.keymap.set("n", "<leader>nl", function() require("noice").cmd "last" end, { desc = "Show the last message" })
-vim.keymap.set("n", "<leader>ne", function() require("noice").cmd "errors" end, { desc = "Show errors" })
-vim.keymap.set(
-  "n",
-  "<leader>nt",
-  function() require("noice").cmd "telescope" end,
-  { desc = "Open messages with Telescope" }
+  { desc = "Show all diagnostic messesages" }
 )
 
 -- Misc
-vim.keymap.set({ "n", "v" }, "<leader>p", [["*p]])
-vim.keymap.set({ "n", "v" }, "<leader>y", [["*y]])
+vim.keymap.set({ "n", "v" }, "<leader>p", [["*p]], { desc = "Paste from system clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>y", [["*y]], { desc = "Yank to system clipboard" })
 vim.keymap.set("n", "<leader><leader>l", function() vim.cmd [[luafile %]] end, { desc = "Load the current lua file" })
 
 -- Movement
 vim.keymap.set("n", "j", [[v:count || mode(1)[0:1] == "no" ? "j" : "gj" ]], { expr = true })
 vim.keymap.set("n", "k", [[v:count || mode(1)[0:1] == "no" ? "k" : "gk" ]], { expr = true })
+
+-- Fold remap to always show indent-blankline
+local fold_keymaps = { "zo", "zO", "zc", "zC", "za", "zA", "zv", "zx", "zX", "zm", "zM", "zr", "zR" }
+for _, keymap in pairs(fold_keymaps) do
+  vim.keymap.set({ "n" }, keymap, keymap .. "<cmd>IndentBlanklineRefresh<CR>")
+end
