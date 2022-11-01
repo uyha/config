e3c5c96a0fe15d7dc5f086b6b61cef5dc17906c3
Uy Ha <hchanuy@gmail.com>
2022-11-02 00:28:28 +0100

    add source for cmdline

--- nvim/lua/bt/plugins/nvim-cmp.lua
+++ nvim/lua/bt/plugins/nvim-cmp.lua
@@ -31,6 +31,18 @@ M.setup = function()
       { name = "path" },
     },
   }
+
+  cmp.setup.cmdline({ "/", "?" }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = "buffer" },
+    },
+  })
+
+  cmp.setup.cmdline(":", {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
+  })
 end
 
 return M
