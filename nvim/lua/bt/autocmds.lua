6c2beda73558e3ba8f8e3c5f069b4ddafae10030
Uy Ha <hchanuy@gmail.com>
2022-11-01 23:39:03 +0100

    fix quickfix autocmd and kepmap

--- nvim/lua/bt/autocmds.lua
+++ nvim/lua/bt/autocmds.lua
@@ -1,12 +1,13 @@
 -- Close quickfix with 'q' and automatically close quickfix when leave it
-vim.api.nvim_create_autocmd("FileType", {
+vim.api.nvim_create_autocmd({ "FileType" }, {
   group = vim.api.nvim_create_augroup("BTQuickfixKeyMap", {}),
   callback = function(params)
-    if params.file == "qf" then
-      vim.keymap.set("n", "q", function() vim.cmd [[cclose]] end, {})
+    if vim.bo.filetype == "qf" then
+      vim.keymap.set("n", "q", function() vim.api.nvim_win_close(0, true) end, { buffer = params.buf })
       vim.api.nvim_create_autocmd("WinLeave", {
         group = vim.api.nvim_create_augroup("BTQuickfixAutoClose", {}),
-        callback = function() vim.cmd [[cclose]] end,
+        callback = function() vim.api.nvim_win_close(0, true) end,
+        buffer = params.buf,
       })
     end
   end,
