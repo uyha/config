dfc496815de64013b035a3b0a1855f2984aca642
Uy Ha <hchanuy@gmail.com>
2022-11-02 21:49:22 +0100

    press q to quick help

--- nvim/lua/bt/autocmds.lua
+++ nvim/lua/bt/autocmds.lua
@@ -12,3 +12,12 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     end
   end,
 })
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  group = vim.api.nvim_create_augroup("BTHelpKeyMap", {}),
+  callback = function(params)
+    if vim.bo.filetype == "help" then
+      vim.keymap.set("n", "q", function() vim.api.nvim_win_close(0, true) end, { buffer = params.buf })
+    end
+  end,
+})
