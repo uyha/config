ad6177b42abf16d0030c84ebf84f97b25b049790
Uy Ha <hchanuy@gmail.com>
2022-11-04 15:02:40 +0100

    refactor(nvim): arrange plugins into blocks of functionality

--- nvim/lua/bt/plugins/init.lua
+++ nvim/lua/bt/plugins/init.lua
@@ -18,20 +18,13 @@ local startup = function(use)
   }
   use { "lewis6991/impatient.nvim" }
 
-  use {
-    "nvim-tree/nvim-tree.lua",
-    requires = { "nvim-tree/nvim-web-devicons" },
-    config = function() require("nvim-tree").setup {} end,
-    cmd = { "NvimTreeToggle", "NvimTreeFindFile", "NvimTreeRefresh" },
-  }
+  -- UI Improvements
   use {
     "feline-nvim/feline.nvim",
     config = function() require("bt.plugins.feline").setup() end,
     after = { "nvim-navic", "catppuccin" },
   }
-
   use { "SmiteshP/nvim-navic" }
-
   use {
     "catppuccin/nvim",
     as = "catppuccin",
@@ -40,54 +33,33 @@ local startup = function(use)
       vim.cmd [[colorscheme catppuccin-mocha]]
     end,
   }
+  use { "NvChad/nvim-colorizer.lua", config = function() require("colorizer").setup() end }
+  use {
+    "lukas-reineke/indent-blankline.nvim",
+    config = function() require("bt.plugins.indent-blankline").config() end,
+    after = { "nvim-treesitter", "catppuccin" },
+  }
+  use { "kevinhwang91/nvim-bqf", ft = "qf" }
 
+  -- UI Components
+  use {
+    "nvim-tree/nvim-tree.lua",
+    requires = { "nvim-tree/nvim-web-devicons" },
+    config = function() require("nvim-tree").setup {} end,
+    cmd = { "NvimTreeToggle", "NvimTreeFindFile", "NvimTreeRefresh" },
+  }
+  use {
+    "folke/noice.nvim",
+    config = function() require("bt.plugins.noice").config() end,
+    event = { "UIEnter" },
+    requires = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
+  }
   use {
     "akinsho/toggleterm.nvim",
     config = function() require("bt.plugins.toggleterm").setup() end,
     keys = {
       { "n", "<leader>lzg" },
       { "n", "<M-t>" },
-    },
-  }
-
-  use {
-    "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
-    config = function() require("bt.plugins.nvim-treesitter").setup() end,
-    event = { "BufRead", "BufWinEnter", "BufNewFile" },
-  }
-
-  use {
-    "lewis6991/gitsigns.nvim",
-    module = "gitsigns",
-    setup = require("bt.plugins.gitsigns").setup,
-    config = require("bt.plugins.gitsigns").config,
-  }
-  use {
-    "numToStr/Comment.nvim",
-    config = function() require("Comment").setup() end,
-    keys = {
-      { "n", "gc" },
-      { "n", "gb" },
-      { "v", "gc" },
-      { "v", "gb" },
-    },
-  }
-  use {
-    "jose-elias-alvarez/null-ls.nvim",
-    module = "null-ls",
-    requires = { "nvim-lua/plenary.nvim" },
-    setup = require("bt.plugins.null-ls").setup,
-    config = require("bt.plugins.null-ls").config,
-  }
-
-  use {
-    "folke/noice.nvim",
-    config = function() require("bt.plugins.noice").config() end,
-    event = { "UIEnter" },
-    requires = {
-      "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
     },
   }
   use {
@@ -102,41 +74,13 @@ local startup = function(use)
     },
   }
 
+  -- Git Integration
   use {
-    "folke/which-key.nvim",
-    config = function() require("which-key").setup() end,
+    "lewis6991/gitsigns.nvim",
+    module = "gitsigns",
+    setup = require("bt.plugins.gitsigns").setup,
+    config = require("bt.plugins.gitsigns").config,
   }
-
-  use {
-    "hrsh7th/nvim-cmp",
-    config = function() require("bt.plugins.nvim-cmp").setup() end,
-    event = { "InsertEnter" },
-  }
-
-  use {
-    "L3MON4D3/LuaSnip",
-    config = function() require("bt.plugins.luasnip").setup() end,
-    after = { "nvim-cmp" },
-  }
-  use { "saadparwaiz1/cmp_luasnip", after = { "LuaSnip" } }
-
-  use { "hrsh7th/cmp-nvim-lsp", after = { "cmp_luasnip" } }
-  use { "hrsh7th/cmp-buffer", after = { "cmp-nvim-lsp" } }
-  use { "hrsh7th/cmp-path", after = { "cmp-buffer" } }
-  use { "hrsh7th/cmp-cmdline", after = { "cmp-path" } }
-
-  use {
-    "neovim/nvim-lspconfig",
-    config = function() require("bt.plugins.nvim-lspconfig").setup() end,
-    after = { "nvim-navic" },
-  }
-
-  use {
-    "lukas-reineke/indent-blankline.nvim",
-    config = function() require("bt.plugins.indent-blankline").config() end,
-    after = { "nvim-treesitter", "catppuccin" },
-  }
-
   use {
     "sindrets/diffview.nvim",
     requires = { "nvim-lua/plenary.nvim" },
@@ -150,6 +94,26 @@ local startup = function(use)
     },
   }
 
+  -- Treesitter and friends
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+    config = function() require("bt.plugins.nvim-treesitter").setup() end,
+    event = { "BufRead", "BufWinEnter", "BufNewFile" },
+  }
+  use { "p00f/nvim-ts-rainbow", after = { "nvim-treesitter" } }
+
+  -- Editing
+  use {
+    "numToStr/Comment.nvim",
+    config = function() require("Comment").setup() end,
+    keys = {
+      { "n", "gc" },
+      { "n", "gb" },
+      { "v", "gc" },
+      { "v", "gb" },
+    },
+  }
   use {
     "kylechui/nvim-surround",
     config = function() require("nvim-surround").setup {} end,
@@ -161,7 +125,31 @@ local startup = function(use)
       { "v", "S" },
     },
   }
+  use { "windwp/nvim-autopairs", config = function() require("nvim-autopairs").setup {} end, event = "InsertEnter" }
 
+  -- Language servers and completions
+  use {
+    "jose-elias-alvarez/null-ls.nvim",
+    module = "null-ls",
+    requires = { "nvim-lua/plenary.nvim" },
+    setup = require("bt.plugins.null-ls").setup,
+    config = require("bt.plugins.null-ls").config,
+  }
+  use {
+    "neovim/nvim-lspconfig",
+    config = function() require("bt.plugins.nvim-lspconfig").setup() end,
+    after = { "nvim-navic" },
+  }
+  use { "hrsh7th/nvim-cmp", config = function() require("bt.plugins.nvim-cmp").setup() end, event = { "InsertEnter" } }
+  use { "L3MON4D3/LuaSnip", config = function() require("bt.plugins.luasnip").setup() end, after = { "nvim-cmp" } }
+  use { "saadparwaiz1/cmp_luasnip", after = { "LuaSnip" } }
+  use { "hrsh7th/cmp-nvim-lsp", after = { "cmp_luasnip" } }
+  use { "hrsh7th/cmp-buffer", after = { "cmp-nvim-lsp" } }
+  use { "hrsh7th/cmp-path", after = { "cmp-buffer" } }
+  use { "hrsh7th/cmp-cmdline", after = { "cmp-path" } }
+
+  -- Miscellaneous
+  use { "folke/which-key.nvim", config = function() require("which-key").setup() end }
   use {
     "aserowy/tmux.nvim",
     config = function() require("tmux").setup() end,
@@ -172,15 +160,6 @@ local startup = function(use)
       { "n", "<C-l>" },
     },
   }
-
-  use {
-    "p00f/nvim-ts-rainbow",
-    after = { "nvim-treesitter" },
-  }
-
-  use { "kevinhwang91/nvim-bqf", ft = "qf" }
-  use { "windwp/nvim-autopairs", config = function() require("nvim-autopairs").setup {} end, event = "InsertEnter" }
-  use { "NvChad/nvim-colorizer.lua", config = function() require("colorizer").setup() end }
 end
 
 local config = {
