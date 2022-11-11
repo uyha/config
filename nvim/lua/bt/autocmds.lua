803a8f298211d16223f9ff42e37dfe3fb932b4fc
Uy Ha <hchanuy@gmail.com>
2022-11-11 13:02:00 +0000

    feat(nvim): automatically close Trouble when exiting it

--- nvim/lua/bt/autocmds.lua
+++ nvim/lua/bt/autocmds.lua
@@ -2,7 +2,8 @@
 vim.api.nvim_create_autocmd({ "FileType" }, {
   group = vim.api.nvim_create_augroup("BTQuickfixKeyMap", {}),
   callback = function(params)
-    if vim.bo.filetype == "qf" then
+    local filetype = vim.bo.filetype
+    if filetype == "qf" or filetype == "Trouble" then
       vim.keymap.set("n", "q", function() vim.api.nvim_win_close(0, true) end, { buffer = params.buf })
       vim.api.nvim_create_autocmd("WinLeave", {
         group = vim.api.nvim_create_augroup("BTQuickfixAutoClose", {}),
