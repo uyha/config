9b5a7a580296b39196968eff77517b7b01ec5a2b
Uy Ha <hchanuy@gmail.com>
2022-11-14 15:04:05 +0000

    feat(zsh): add aliases for unsafe-{ssh,scp}

--- zsh/conf.d/interactive/001-aliases.zsh
+++ zsh/conf.d/interactive/001-aliases.zsh
@@ -15,6 +15,8 @@ command -v bat &>/dev/null && alias cat="bat"
 command -v lazygit &>/dev/null && alias lzg="lazygit"
 command -v fd &>/dev/null && alias fa="fd --no-ignore --hidden"
 command -v explorer.exe &>/dev/null && alias explorer="explorer.exe"
+command -v ssh &>/dev/null && alias unsafe-ssh="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+command -v scp &>/dev/null && alias unsafe-scp="scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 
 if command -v git &>/dev/null; then
   alias g="git"
