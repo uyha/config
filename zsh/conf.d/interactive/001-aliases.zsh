08ecf1196707a150934804b57cab8010be99c10c
Uy Ha <hchanuy@gmail.com>
2022-11-16 11:49:39 +0000

    feat(zsh): alias du and top to dust and btm, also refactor code to use function exists

--- zsh/conf.d/interactive/001-aliases.zsh
+++ zsh/conf.d/interactive/001-aliases.zsh
@@ -1,4 +1,8 @@
-if command -v exa &>/dev/null; then
+exists() {
+  command -v "$1" &>/dev/null
+}
+
+if exists exa; then
   alias ls="exa"
   alias tree="ls --tree"
 fi
@@ -10,16 +14,20 @@ alias rm="rm -I"
 alias mv="mv -i"
 alias cp="cp -i"
 
-command -v nvim &>/dev/null && alias vim="nvim"
-command -v bat &>/dev/null && alias cat="bat"
-command -v lazygit &>/dev/null && alias lzg="lazygit"
-command -v fd &>/dev/null && alias fa="fd --no-ignore --hidden"
-command -v explorer.exe &>/dev/null && alias explorer="explorer.exe"
-command -v ssh &>/dev/null && alias unsafe-ssh="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
-command -v scp &>/dev/null && alias unsafe-scp="scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+exists nvim && alias vim="nvim"
+exists bat && alias cat="bat"
+exists lazygit && alias lzg="lazygit"
+exists fd && alias fa="fd --no-ignore --hidden"
+exists explorer.exe && alias explorer="explorer.exe"
+exists ssh && alias unsafe-ssh="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+exists scp && alias unsafe-scp="scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+exists dust && alias du="dust"
+exists btm && alias top="btm"
 
-if command -v git &>/dev/null; then
+if exists git; then
   alias g="git"
   alias pgit="GIT_SSH_COMMAND='ssh -i ~/.ssh/personal' git"
   alias wgit="GIT_SSH_COMMAND='ssh -i ~/.ssh/work' git"
 fi
+
+unfunction exists
