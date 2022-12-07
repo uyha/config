4448593c4cce14d22fa1d1e8024b2e51cd2d6d1a
Uy Ha <hchanuy@gmail.com>
2022-12-07 14:06:18 +0000

    refactor(nvim): change how filetype is checked in autocmds.lua

--- nvim/lua/bt/autocmds.lua
+++ nvim/lua/bt/autocmds.lua
@@ -3,17 +3,23 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   group = vim.api.nvim_create_augroup("MyAutoCmdForFileTypes", {}),
   callback = function(params)
     local filetype = vim.bo.filetype
-    if filetype == "qf" or filetype == "Trouble" then
-      vim.keymap.set("n", "q", function() vim.api.nvim_win_close(0, true) end, { buffer = params.buf })
-      vim.api.nvim_create_autocmd("WinLeave", {
-        group = vim.api.nvim_create_augroup("BTQuickfixAutoClose", {}),
-        callback = function() vim.api.nvim_win_close(0, true) end,
-        buffer = params.buf,
-      })
+    for _, ft in pairs { "qf", "Trouble" } do
+      if filetype == ft then
+        vim.keymap.set("n", "q", function() vim.api.nvim_win_close(0, true) end, { buffer = params.buf })
+        vim.api.nvim_create_autocmd("WinLeave", {
+          group = vim.api.nvim_create_augroup("BTQuickfixAutoClose", {}),
+          callback = function() vim.api.nvim_win_close(0, true) end,
+          buffer = params.buf,
+        })
+        break
+      end
     end
 
-    if vim.bo.filetype == "help" or vim.bo.filetype == "checkhealth" or vim.bo.filetype == "tsplayground" then
-      vim.keymap.set("n", "q", function() vim.api.nvim_win_close(0, true) end, { buffer = params.buf })
+    for _, ft in pairs { "help", "checkhealth", "tsplayground" } do
+      if filetype == ft then
+        vim.keymap.set("n", "q", function() vim.api.nvim_win_close(0, true) end, { buffer = params.buf })
+        break
+      end
     end
 
     if vim.bo.filetype == "lua" then
