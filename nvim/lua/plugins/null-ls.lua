341f8de9b1af2ad4f5993e570d761e9aa9e96d5d
Uy Ha <hchanuy@gmail.com>
2023-02-24 13:03:34 +0100

    feat(nvim): add back null-ls config from previous config

---
+++ nvim/lua/plugins/null-ls.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function()
+      local nls = require("null-ls")
+      local builtins = nls.builtins
+      local formatting = builtins.formatting
+      local diagnostics = builtins.diagnostics
+      local code_actions = builtins.code_actions
+
+      return {
+        sources = {
+          -- Web stuff
+          formatting.prettier.with({ extra_filetypes = { "svelte" } }),
+
+          -- Lua
+          formatting.stylua,
+          diagnostics.luacheck.with({ extra_args = { "--globals", "vim" } }),
+
+          -- Shell
+          formatting.shfmt.with({ extra_args = { "-i", "2" } }),
+          code_actions.shellcheck.with({}),
+
+          -- C/C++
+
+          formatting.clang_format,
+
+          -- Python
+          formatting.black,
+          diagnostics.flake8,
+
+          -- Perl
+          formatting.perltidy,
+
+          -- Rust
+          formatting.rustfmt,
+        },
+      }
+    end,
+  },
+}
