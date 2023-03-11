e03d12f7268192380420e3d4058b74a7b475a5ee
Uy Ha <hchanuy@gmail.com>
2023-03-11 14:49:29 +0100

    feat(nvim): remove <leader>e in terminal mode

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -14,7 +14,7 @@ return {
     end,
     cmd = { "NvimTreeRefresh" },
     keys = {
-      { "<leader>e", function() vim.cmd([[NvimTreeToggle]]) end, mode = { "n", "t" }, desc = "Toggle nvim-tree" },
+      { "<leader>e", function() vim.cmd([[NvimTreeToggle]]) end, desc = "Toggle nvim-tree" },
       { "<C-f>", function() vim.cmd([[NvimTreeFindFile]]) end, desc = "Show current file in nvim-tree" },
     },
   },
