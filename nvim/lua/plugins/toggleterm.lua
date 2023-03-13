3d9a57b286e1026815d19d6c53a57217a7e78137
Uy Ha <hchanuy@gmail.com>
2023-03-13 09:12:20 +0000

    feat(nvim): use q to toggle main terminal, remove obsolete mapping

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -11,13 +11,12 @@ return {
       on_create = function(term)
         local config = { buffer = term.bufnr, silent = true }
 
-        vim.keymap.set({ "t", "n" }, "<M-t>", function() term:toggle() end, config)
-
         vim.keymap.set("t", "<Esc>", termesc(), config)
         vim.keymap.set("t", "<C-h>", termesc("<C-w>h"), config)
         vim.keymap.set("t", "<C-j>", termesc("<C-w>j"), config)
         vim.keymap.set("t", "<C-k>", termesc("<C-w>k"), config)
         vim.keymap.set("t", "<C-l>", termesc("<C-w>l"), config)
+        vim.keymap.set("n", "q", function() term:toggle() end, config)
       end,
     })
     local lazygit = Terminal:new({
