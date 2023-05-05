51e1bbc354b9fc4de04e403b35e788d9e62a95d4
Uy Ha <hchanuy@gmail.com>
2023-05-05 16:04:30 +0300

    feat(zsh): prevent zsh expanding to literal for glob expression

--- zsh/conf.d/interactive/005-options.zsh
+++ zsh/conf.d/interactive/005-options.zsh
@@ -1,6 +1,7 @@
 zstyle ':completion:*' menu yes select
 zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*' # Allow case insensitive matching
 
-setopt SHARE_HISTORY
-setopt HIST_IGNORE_ALL_DUPS
-setopt AUTO_CD
+setopt share_history
+setopt hist_ignore_all_dups
+setopt auto_cd
+setopt null_glob
