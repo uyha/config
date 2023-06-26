24bc1e52aa490e2d0ec19832898697a810cfae99
Uy Ha <hchanuy@gmail.com>
2023-06-26 14:03:48 +0000

    move callbacks to reduce duplicates

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -11,22 +11,14 @@ return {
           },
         },
       })
-      vim.keymap.set(
-        "n",
-        "<leader>e",
-        function() require("nvim-tree.api").tree.toggle() end,
-        { desc = "Toggle nvim-tree" }
-      )
-      vim.keymap.set(
-        "n",
-        "<M-f>",
-        function() require("nvim-tree.api").tree.find_file({ open = true, focus = true }) end,
-        { desc = "Show current file in nvim-tree" }
-      )
     end,
     keys = {
-      { "<leader>e", desc = "Toggle nvim-tree" },
-      { "<M-f>", desc = "Show current file in nvim-tree" },
+      { "<leader>e", function() require("nvim-tree.api").tree.toggle() end, desc = "Toggle nvim-tree" },
+      {
+        "<M-f>",
+        function() require("nvim-tree.api").tree.find_file({ open = true, focus = true }) end,
+        desc = "Show current file in nvim-tree",
+      },
     },
   },
   {
