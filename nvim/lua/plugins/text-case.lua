d8c88ff20e95d5d739b7048f7854d6fd83ce5fe4
Uy Ha <hchanuy@gmail.com>
2023-07-01 16:33:36 +0000

    feat(nvim): allow changing case in visual mode

--- nvim/lua/plugins/text-case.lua
+++ nvim/lua/plugins/text-case.lua
@@ -1,10 +1,35 @@
 return {
   "johmsalas/text-case.nvim",
   keys = {
-    { "ccs", function() require("textcase").current_word("to_snake_case") end, desc = "Change word to snake case" },
-    { "cc-", function() require("textcase").current_word("to_dash_case") end, desc = "Change word to dash case" },
-    { "ccm", function() require("textcase").current_word("to_constant_case") end, desc = "Change word to macro case" },
-    { "ccc", function() require("textcase").current_word("to_camel_case") end, desc = "Change word to camel case" },
-    { "ccp", function() require("textcase").current_word("to_path_case") end, desc = "Change word to path case" },
+    {
+      "ccs",
+      function() require("textcase").current_word("to_snake_case") end,
+      mode = { "n", "v" },
+      desc = "Change word to snake case",
+    },
+    {
+      "cc-",
+      function() require("textcase").current_word("to_dash_case") end,
+      mode = { "n", "v" },
+      desc = "Change word to dash case",
+    },
+    {
+      "ccm",
+      function() require("textcase").current_word("to_constant_case") end,
+      mode = { "n", "v" },
+      desc = "Change word to macro case",
+    },
+    {
+      "ccc",
+      function() require("textcase").current_word("to_camel_case") end,
+      mode = { "n", "v" },
+      desc = "Change word to camel case",
+    },
+    {
+      "ccp",
+      function() require("textcase").current_word("to_path_case") end,
+      mode = { "n", "v" },
+      desc = "Change word to path case",
+    },
   },
 }
