98856c5bc47e99f584cc5e86aa3b9941c27ac2f4
Uy Ha <hchanuy@gmail.com>
2023-07-01 19:54:46 +0000

    fix(nvim): use <leader>c for changing case

--- nvim/lua/plugins/text-case.lua
+++ nvim/lua/plugins/text-case.lua
@@ -2,31 +2,31 @@ return {
   "johmsalas/text-case.nvim",
   keys = {
     {
-      "ccs",
+      "<leader>cs",
       function() require("textcase").current_word("to_snake_case") end,
       mode = { "n", "v" },
       desc = "Change word to snake case",
     },
     {
-      "cc-",
+      "<leader>c-",
       function() require("textcase").current_word("to_dash_case") end,
       mode = { "n", "v" },
       desc = "Change word to dash case",
     },
     {
-      "ccm",
+      "<leader>cm",
       function() require("textcase").current_word("to_constant_case") end,
       mode = { "n", "v" },
       desc = "Change word to macro case",
     },
     {
-      "ccc",
+      "<leader>cc",
       function() require("textcase").current_word("to_camel_case") end,
       mode = { "n", "v" },
       desc = "Change word to camel case",
     },
     {
-      "ccp",
+      "<leader>cp",
       function() require("textcase").current_word("to_path_case") end,
       mode = { "n", "v" },
       desc = "Change word to path case",
