c69e2cb521b0ed520c74b65203c8c1e2274d4266
Uy Ha <hchanuy@gmail.com>
2023-07-16 16:09:41 +0000

    feat(nvim): configure nvim-dap-ui layout

---
+++ nvim/lua/plugins/dap.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "rcarriga/nvim-dap-ui",
+    opts = {
+      layouts = {
+        {
+          elements = {
+            {
+              id = "stacks",
+              size = 0.15,
+            },
+            {
+              id = "repl",
+              size = 0.30,
+            },
+            {
+              id = "scopes",
+              size = 0.30,
+            },
+            {
+              id = "console",
+              size = 0.25,
+            },
+          },
+          position = "bottom",
+          size = 10,
+        },
+      },
+    },
+  },
+}
