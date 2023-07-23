94b097feba81f20b0103005024a6c5740a49e215
Uy Ha <hchanuy@gmail.com>
2023-07-23 18:31:50 +0000

    feat(nvim): add back Tab and Shift+Tab for selecting completion item

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,18 +1,43 @@
 return {
   "hrsh7th/nvim-cmp",
+  ---@param opts cmp.ConfigSchema
   opts = function(_, opts)
+    local cmp = require("cmp")
     local compare = require("cmp.config.compare")
+
     opts.sorting.comparators = {
       compare.offset,
       compare.exact,
       compare.score,
+      ---@diagnostic disable-next-line: assign-type-mismatch
       compare.recently_used,
+      ---@diagnostic disable-next-line: assign-type-mismatch
       compare.locality,
       compare.kind,
       compare.sort_text,
       compare.length,
       compare.order,
     }
+
+    opts.mapping = vim.tbl_extend("force", opts.mapping, {
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+      ["<C-o>"] = cmp.mapping(function(fallback)
+        if not cmp.complete() then fallback() end
+      end, { "i", "s" }),
+    })
     return opts
   end,
 }
--- nvim/lua/plugins/override.lua
+++ nvim/lua/plugins/override.lua
@@ -15,17 +15,4 @@ return {
       { "<leader><space>", require("lazyvim.util").telescope("files", { cwd = false }), desc = "Find Files (cwd)" },
     },
   },
-  {
-    "hrsh7th/nvim-cmp",
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      local cmp = require("cmp")
-
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<C-o>"] = cmp.mapping(function(fallback)
-          if not cmp.complete() then fallback() end
-        end, { "i", "s" }),
-      })
-    end,
-  },
 }
