0c7af1e00b800f0e20e4194c20d58fc9c08328f5
Uy Ha <hchanuy@gmail.com>
2023-09-07 08:14:56 +0000

    override clangd to not insert header in iwyu style

---
+++ nvim/lua/plugins/clangd.lua
@@ -0,0 +1,18 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      clangd = {
+        cmd = {
+          "clangd",
+          "--background-index",
+          "--clang-tidy",
+          "--header-insertion=never",
+          "--completion-style=detailed",
+          "--function-arg-placeholders",
+          "--fallback-style=llvm",
+        },
+      },
+    },
+  },
+}
