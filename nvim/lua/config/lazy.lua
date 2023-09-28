757db2bb7b3c707beb937ef084d574d4bf0b75b9
Uy Ha <hchanuy@gmail.com>
2023-09-28 08:26:09 +0000

    enable extras for json

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -16,6 +16,7 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
+    { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "plugins" },
