2de9d353997e1d2dd17feb9645783444d452733e
Uy Ha <hchanuy@gmail.com>
2023-10-19 07:15:14 +0000

    customize `nvim-window-picker`
    - Having hint as big floating letter on the buffer instead of status line
    - Use normal alphabet for selecting the buffer
    - Ignore 'nofile' buffer type so the progress bar is not considered

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -4,12 +4,14 @@ return {
     "s1n7ax/nvim-window-picker",
     config = function()
       require("window-picker").setup({
+        hint = "floating-big-letter",
+        selection_chars = "ABCDEFG",
         filter_rules = {
           include_current_win = false,
           autoselect_one = true,
           bo = {
             filetype = { "neo-tree", "neo-tree-popup", "notify" },
-            buftype = { "terminal", "quickfix" },
+            buftype = { "terminal", "quickfix", "nofile" },
           },
         },
       })
@@ -32,6 +34,7 @@ return {
     },
     window = {
       mappings = {
+        ["<cr>"] = "open_with_window_picker",
         ["<C-x>"] = "split_with_window_picker",
         ["<C-v>"] = "vsplit_with_window_picker",
       },
