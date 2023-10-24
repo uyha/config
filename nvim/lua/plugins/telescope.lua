998dbe787ad9728d3ec97581f5ee6ea82129ccf2
Uy Ha <hchanuy@gmail.com>
2023-10-24 13:56:55 +0000

    add telescope-file-browser

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,15 +1,24 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  dependencies = { "nvim-tree/nvim-tree.lua" },
-  keys = {
-    {
-      "<leader>fa",
-      function()
-        require("telescope.builtin").find_files({
-          find_command = { "fd", "--no-ignore", "--hidden", "--exclude", ".git", "--exclude", ".idea" },
-        })
-      end,
-      desc = "Fuzzy find everything",
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      {
+        "<leader>fa",
+        function()
+          require("telescope.builtin").find_files({
+            find_command = { "fd", "--no-ignore", "--hidden", "--exclude", ".git", "--exclude", ".idea" },
+          })
+        end,
+        desc = "Fuzzy find everything",
+      },
+    },
+  },
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    config = function() require("telescope").load_extension("file_browser") end,
+    keys = {
+      { "<leader>fB", "<cmd>Telescope file_browser<CR>", desc = "Open Telescope File Browser" },
     },
   },
 }
