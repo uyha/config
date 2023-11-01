123f1df824d729ff93ce9598bd34eabeae1a37d2
Uy Ha <hchanuy@gmail.com>
2023-11-01 12:17:46 +0000

    more configurations for neorg

--- nvim/lua/plugins/neorg.lua
+++ nvim/lua/plugins/neorg.lua
@@ -1,10 +1,16 @@
 return {
   "nvim-neorg/neorg",
+  dependencies = {
+    "folke/zen-mode.nvim",
+  },
   ft = "norg",
   opts = {
     load = {
       ["core.defaults"] = {},
       ["core.concealer"] = {},
+      ["core.completion"] = { config = { engine = "nvim-cmp" } },
+      ["core.presenter"] = { config = { zen_mode = "zen-mode" } },
+      ["core.keybinds"] = { config = { neorg_leader = " " } },
     },
   },
 }
