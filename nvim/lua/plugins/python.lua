798d91cace0ba3d6c922f21086d5f99aeb2f80c8
Uy Ha <hchanuy@gmail.com>
2024-01-14 11:48:48 +0000

    feat(nvim): add keymap for selecting the previous venv for python projects

---
+++ nvim/lua/plugins/python.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "linux-cultist/venv-selector.nvim",
+    keys = {
+      { "<leader>vc", "<cmd>:VenvSelectCached<cr>", desc = "Select the previous venv for this project" },
+    },
+  },
+}
