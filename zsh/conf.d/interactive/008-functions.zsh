c0d736aa95a91997920bf15722e199e843ffab49
Uy Ha <hchanuy@gmail.com>
2024-02-22 07:48:12 +0000

    feat(zsh): add `docker-nuke` command

--- zsh/conf.d/interactive/008-functions.zsh
+++ zsh/conf.d/interactive/008-functions.zsh
@@ -1,7 +1,29 @@
-function is-wsl(){
+exists() {
+  command -v "$1" &>/dev/null
+}
+
+is-wsl(){
   [[ "$(uname -a | perl -ne 'print lc')" =~ wsl ]]
+}
+
+exists docker && docker-nuke() {
+  echo "This will stop all containers, remove all containers, images, and volumes"
+  read "confirmed?Are you sure you want to continue (y/n)? "
+
+  case "$confirmed" in
+    y)
+      ;;
+    *)
+      return
+      ;;
+  esac
+
+  docker container ls -qa | xargs --no-run-if-empty docker container stop
+  docker system prune --all --volumes --force
 }
 
 is-wsl && function wsl-shutdown() {
   /mnt/c/Windows/System32/wsl.exe --shutdown
 }
+
+unfunction exists
