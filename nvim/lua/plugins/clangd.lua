7bffb65883351d7faa7c79017779bb589880a1f5
Uy Ha <hchanuy@gmail.com>
2024-03-19 22:47:36 +0000

    fix(nvim): properly override clangd settings

--- nvim/lua/plugins/clangd.lua
+++ nvim/lua/plugins/clangd.lua
@@ -3,21 +3,23 @@ return {
   opts = function(_, opts)
     if type(opts.servers) ~= "table" then return end
 
-    vim.tbl_extend("force", opts.servers, {
-      clangd = {
-        cmd = {
-          "clangd",
-          "--background-index",
-          "--clang-tidy",
-          "--header-insertion=never",
-          "--completion-style=detailed",
-          "--function-arg-placeholders",
-          "--fallback-style=llvm",
-        },
-        keys = {
-          { "<leader>cs", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
-        },
-      },
-    })
+    local clangd = opts.servers.clangd or {}
+    clangd.cmd = {
+      "clangd",
+      "--background-index",
+      "--clang-tidy",
+      "--header-insertion=never",
+      "--completion-style=detailed",
+      "--function-arg-placeholders",
+      "--fallback-style=llvm",
+    }
+
+    if type(clangd.keys) == "table" then
+      vim.list_extend(clangd.keys, {
+        { "<leader>cs", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+      })
+    end
+
+    opts.servers.clangd = clangd
   end,
 }
