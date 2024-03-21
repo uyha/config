192c792a0008312b5d9187a095cd65b3f128857a
Uy Ha <hchanuy@gmail.com>
2024-03-21 11:02:52 +0000

    chore(nvim): remove custom just setup for nvim-treesitter (upstream already has it)

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,23 +1,6 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      {
-        "IndianBoy42/tree-sitter-just",
-        ft = "just",
-        config = function()
-          ---@diagnostic disable-next-line: inject-field
-          require("nvim-treesitter.parsers").get_parser_configs().just = {
-            install_info = {
-              url = "https://github.com/IndianBoy42/tree-sitter-just", -- local path or git repo
-              files = { "src/parser.c", "src/scanner.c" },
-              branch = "main",
-            },
-            maintainers = { "@IndianBoy42" },
-          }
-        end,
-      },
-    },
     opts = function(_, opts)
       if type(opts.ensure_installed) == "table" then
         vim.list_extend(opts.ensure_installed, {
@@ -25,7 +8,6 @@ return {
           "html",
           "javascript",
           "json",
-          "just",
           "lua",
           "markdown",
           "markdown_inline",
