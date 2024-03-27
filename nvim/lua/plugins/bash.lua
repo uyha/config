aad436c047c9ed712f60e687c0e74dc4863a5648
Uy Ha <hchanuy@gmail.com>
2024-03-27 15:56:08 +0000

    feat(nvim): install needed tools for bash

--- nvim/lua/plugins/bash.lua
+++ nvim/lua/plugins/bash.lua
@@ -1,16 +1,26 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = function(_, opts)
-      if type(opts.servers) == "table" then vim.list_extend(opts.servers, {
+    opts = {
+      servers = {
         bashls = {},
-      }) end
-    end,
+      },
+    },
   },
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       if type(opts.ensure_installed) ~= table then vim.list_extend(opts.ensure_installed, { "bash" }) end
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) ~= "table" then return end
+
+      vim.list_extend(opts.ensure_installed, {
+        "bash-language-server",
+      })
     end,
   },
 }
