f415becec7e7ce9a40c9c1a768977f541f49f2be
Uy Ha <hchanuy@gmail.com>
2024-03-28 12:43:27 +0000

    fix(nvim): fix svelte config

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -5,13 +5,9 @@ return {
 
     local nls = require("null-ls")
     local builtins = nls.builtins
-    local formatting = builtins.formatting
     local diagnostics = builtins.diagnostics
 
     vim.list_extend(opts.sources, {
-      -- Web stuff
-      formatting.prettierd.with({ extra_filetypes = { "svelte" } }),
-
       -- Github Action
       diagnostics.actionlint,
     })
