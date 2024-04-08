9111a5f5beddd79256eac616974639a5e4092675
Uy Ha <hchanuy@gmail.com>
2024-04-08 14:54:25 +0000

    fix(nvim): using spaces for indenting bash files

--- nvim/lua/plugins/bash.lua
+++ nvim/lua/plugins/bash.lua
@@ -23,4 +23,16 @@ return {
       })
     end,
   },
+  {
+    "nvimtools/none-ls.nvim",
+    opts = function(_, opts)
+      if type(opts.sources) ~= "table" then return end
+
+      local nls = require("null-ls")
+
+      vim.list_extend(opts.sources, {
+        nls.builtins.formatting.shfmt.with({ extra_args = { "-i", "2" } }),
+      })
+    end,
+  },
 }
