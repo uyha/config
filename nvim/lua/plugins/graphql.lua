0271b7e61ba1288306d8f92518929421451ce70c
Uy Ha <hchanuy@gmail.com>
2024-05-08 10:58:56 +0300

    feat(graphql): add support for `graphql`

---
+++ nvim/lua/plugins/graphql.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        graphql = {},
+      },
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) ~= "table" then return end
+
+      vim.list_extend(opts.ensure_installed, {
+        "graphql-language-service-cli",
+      })
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        ["graphql"] = { "prettierd" },
+      },
+    },
+  },
+}
