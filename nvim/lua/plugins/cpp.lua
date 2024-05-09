a0de8bcb4693cc814f5877ad44fff797a2eb6d44
Uy Ha <hchanuy@gmail.com>
2024-05-09 13:47:27 +0300

    feat(nvim): install clang-format

--- nvim/lua/plugins/cpp.lua
+++ nvim/lua/plugins/cpp.lua
@@ -1,25 +1,37 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = function(_, opts)
-    if type(opts.servers) ~= "table" then return end
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      if type(opts.servers) ~= "table" then return end
 
-    local clangd = opts.servers.clangd or {}
-    clangd.cmd = {
-      "clangd",
-      "--background-index",
-      "--clang-tidy",
-      "--header-insertion=never",
-      "--completion-style=detailed",
-      "--function-arg-placeholders",
-      "--fallback-style=llvm",
-    }
+      local clangd = opts.servers.clangd or {}
+      clangd.cmd = {
+        "clangd",
+        "--background-index",
+        "--clang-tidy",
+        "--header-insertion=never",
+        "--completion-style=detailed",
+        "--function-arg-placeholders",
+        "--fallback-style=llvm",
+      }
 
-    if type(clangd.keys) == "table" then
-      vim.list_extend(clangd.keys, {
-        { "<leader>cs", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+      if type(clangd.keys) == "table" then
+        vim.list_extend(clangd.keys, {
+          { "<leader>cs", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+        })
+      end
+
+      opts.servers.clangd = clangd
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) ~= "table" then return end
+
+      vim.list_extend(opts.ensure_installed, {
+        "clang-format",
       })
-    end
-
-    opts.servers.clangd = clangd
-  end,
+    end,
+  },
 }
