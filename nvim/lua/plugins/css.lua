1d1d9abbab4b08d7eaab12131090104846c2867f
Uy Ha <hchanuy@gmail.com>
2024-05-09 13:49:12 +0300

    fix(nvim): correct css setting

--- nvim/lua/plugins/css.lua
+++ nvim/lua/plugins/css.lua
@@ -10,7 +10,7 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
-      if type(opts.ensure_installed) == table then vim.list_extend(opts.ensure_installed, { "css" }) end
+      if type(opts.ensure_installed) == "table" then vim.list_extend(opts.ensure_installed, { "css" }) end
     end,
   },
 }
