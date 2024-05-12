8fa8810a3ee6b1f46c3ce5106367cf22776d9abd
Uy Ha <hchanuy@gmail.com>
2024-05-12 09:19:50 +0000

    feat(zsh): add locale info

--- zsh/conf.d/global/003-locale.zsh
+++ zsh/conf.d/global/003-locale.zsh
@@ -1,1 +1,13 @@
-export LC_ALL="en_GB.UTF-8"
+declare -A locales
+locales[ubuntu-20.04]=en_US.UTF-8
+locales[ubuntu-24.04]=en_GB.UTF-8
+
+distro=
+if [[ "$(lsb_release -d)" =~ Ubuntu ]]; then
+  distro=ubuntu
+fi
+distro_version="$(lsb_release -r | awk '{print $2}' | xargs)"
+
+if [[ -n "${locales[$distro-$distro_version]}" ]]; then
+  export LC_ALL="${locales[$distro-$distro_version]}"
+fi
