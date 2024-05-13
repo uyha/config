69dc84d400fb1711811f3616bfbc43e599e21f9a
Uy Ha <hchanuy@gmail.com>
2024-05-13 20:26:14 +0000

    feat(nvim): check spelling in `norg` files

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -27,6 +27,11 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   callback = function() vim.bo.shiftwidth = 4 end,
 })
 
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "norg" },
+  callback = function() vim.opt_local.spell = true end,
+})
+
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(args)
     local client = vim.lsp.get_client_by_id(args.data.client_id)
