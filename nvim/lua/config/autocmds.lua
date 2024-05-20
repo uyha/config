54a8db00c950acc008c716dcda4e9869ea0c10dd
Uy Ha <hchanuy@gmail.com>
2024-05-20 17:25:10 +0000

    chore(nvim): add nil check to avoid warning

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -40,6 +40,7 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(args)
     local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client == nil then return end
     if client.server_capabilities.documentSymbolProvider then
       vim.keymap.set(
         "n",
