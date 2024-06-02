84002fc5ef31f76552aa53d158f8aeed357fd32f
Uy Ha <hchanuy@gmail.com>
2024-06-02 16:17:06 +0000

    refactor(nvim): disable typescript related stuff in javascript

--- nvim/lua/plugins/javascript.lua
+++ nvim/lua/plugins/javascript.lua
@@ -1,23 +1,5 @@
 return {
   {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        tsserver = {},
-      },
-    },
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      if type(opts.ensure_installed) ~= "table" then return end
-
-      vim.list_extend(opts.ensure_installed, {
-        "typescript-language-server",
-      })
-    end,
-  },
-  {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
