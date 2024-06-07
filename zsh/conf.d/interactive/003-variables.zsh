69bc255aeb95ac19139d3543955cbeaaee81a4eb
Uy Ha <hchanuy@gmail.com>
2024-06-07 11:18:41 +0000

    refactor(zsh): use if instead of bool and

--- zsh/conf.d/interactive/003-variables.zsh
+++ zsh/conf.d/interactive/003-variables.zsh
@@ -19,7 +19,15 @@ export FZF_DEFAULT_OPTS=" \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 
-[[ -d "$XDG_DATA_HOME/zsh-completions" ]] && export fpath=("$XDG_DATA_HOME/zsh-completions" $fpath)
+if [[ -d "$XDG_DATA_HOME/zsh-completions" ]]; then
+  export fpath=("$XDG_DATA_HOME/zsh-completions" $fpath)
+fi
 
-[[ -d /var/lib/flatpak/exports/share ]] && export XDG_DATA_DIRS="$XDG_DATA_DIRS:/var/lib/flatpak/exports/share"
-[[ -d "$XDG_DATA_HOME/flatpak/exports/share" ]] && export XDG_DATA_DIRS="$XDG_DATA_DIRS:$XDG_DATA_HOME/flatpak/exports/share"
+if [[ -d /var/lib/flatpak/exports/share ]]; then
+  export XDG_DATA_DIRS="$XDG_DATA_DIRS:/var/lib/flatpak/exports/share"
+fi
+
+if [[ -d "$XDG_DATA_HOME/flatpak/exports/share" ]] ; then
+  export XDG_DATA_DIRS="$XDG_DATA_DIRS:$XDG_DATA_HOME/flatpak/exports/share"
+fi
+
