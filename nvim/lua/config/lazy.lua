8e89529712c0191dea3a7d34d5a183d1e64e46a4
Uy Ha <hchanuy@gmail.com>
2024-07-11 13:48:11 +0000

    feat(nvim): enable typescript

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -21,6 +21,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
     { import = "plugins" },
