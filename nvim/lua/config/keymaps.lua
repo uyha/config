f378751b6d3e9d2cb6c7d4cb57fcedde3b69f912
Uy Ha <hchanuy@gmail.com>
2024-07-15 11:34:45 +0000

    feat(nvim): use lazygit config from LazyVim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,9 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.del("n", "<leader>gg")
-vim.keymap.del("n", "<leader>gG")
-vim.keymap.del("n", "<leader>fT")
 vim.keymap.del("t", "<esc><esc>")
 
 vim.keymap.set({ "n", "i", "t" }, "<M-]>", "<cmd>vertical resize +5<CR>", { silent = true })
--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -24,20 +24,12 @@ return {
         vim.keymap.set("n", "q", function() term:toggle() end, config)
       end,
     })
-    local lazygit = Terminal:new({
-      cmd = "lazygit",
-      direction = "float",
-      hidden = true,
-      exit_on_close = true,
-    })
 
     require("toggleterm").setup({})
 
     vim.keymap.set("n", "<leader>ft", function() main:toggle() end, { desc = "Open main terminal" })
-    vim.keymap.set("n", "<leader>fl", function() lazygit:open() end, { desc = "Open lazygit" })
   end,
   keys = {
     { "<leader>ft", desc = "Open main terminal" },
-    { "<leader>fl", desc = "Open lazygit" },
   },
 }
