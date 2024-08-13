8c85de2428d47d4d3dd1e3d3b0a23c5db4c05f31
Uy Ha <hchanuy@gmail.com>
2024-08-13 11:58:09 +0000

    feat: bind A-[ and A-] for moving between words

--- zsh/conf.d/interactive/005-keybindings.zsh
+++ zsh/conf.d/interactive/005-keybindings.zsh
@@ -7,3 +7,5 @@ bindkey '^f' autosuggest-accept
 bindkey '^[[1~' beginning-of-line
 bindkey '^[[4~' end-of-line
 bindkey '^[[3~' delete-char
+bindkey '^[[[' backward-word
+bindkey '^[[]' forward-word
