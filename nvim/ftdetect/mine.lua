f94ec3f65c612682b7134ed2a8fbfd2b90d71b1b
Uy Ha <hchanuy@gmail.com>
2024-09-01 06:59:08 +0000

    feat(nvim): detect files running with `uv` being python files

--- nvim/ftdetect/mine.lua
+++ nvim/ftdetect/mine.lua
@@ -33,5 +33,9 @@ vim.filetype.add({
     [".*/include/.*.inc"] = "bitbake",
     [".*/.config/git/.*"] = "gitconfig",
     ["justfile"] = "just",
+    [".*/.*"] = function(path, bufnr)
+      local content = vim.api.nvim_buf_get_lines(bufnr, 0, 1, false)[1] or ""
+      if vim.regex([[^#!.*uv run python]]):match_str(content) ~= nil then return "python" end
+    end,
   },
 })
