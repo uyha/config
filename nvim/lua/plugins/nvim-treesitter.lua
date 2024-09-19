321872275f6836c55c11827c441a003adee7f560
Uy Ha <hchanuy@gmail.com>
2024-09-19 18:45:28 +0000

    feat(nvim): add `devicetree` parser

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -5,6 +5,7 @@ return {
       if type(opts.ensure_installed) == "table" then
         vim.list_extend(opts.ensure_installed, {
           "dockerfile",
+          "devicetree",
           "html",
           "javascript",
           "json",
