5561e411df8c4e061cb66be6c40a11fe4f140ca7
Uy Ha <hchanuy@gmail.com>
2024-10-17 18:00:52 +0000

    feat(zsh): enable vim keybind

--- zsh/conf.d/interactive/005-keybindings.zsh
+++ zsh/conf.d/interactive/005-keybindings.zsh
@@ -2,10 +2,7 @@ if [[ -f "$HOMEBREW_PREFIX/opt/fzf/shell/key-bindings.zsh" ]]; then
   source "$HOMEBREW_PREFIX/opt/fzf/shell/key-bindings.zsh"
 fi
 
+bindkey -v
+
 bindkey '^[[Z' reverse-menu-complete
 bindkey '^f' autosuggest-accept
-bindkey '^[[1~' beginning-of-line
-bindkey '^[[4~' end-of-line
-bindkey '^[[3~' delete-char
-bindkey '^[[[' backward-word
-bindkey '^[[]' forward-word
