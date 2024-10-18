d1c4bbbba563da38864f21a4d2329c929003a365
Uy Ha <hchanuy@gmail.com>
2024-10-18 11:14:37 +0000

    feat(zsh): bind Ctrl-a to toggle between command and insert mode

--- zsh/conf.d/interactive/005-keybindings.zsh
+++ zsh/conf.d/interactive/005-keybindings.zsh
@@ -3,6 +3,9 @@ if [[ -f "$HOMEBREW_PREFIX/opt/fzf/shell/key-bindings.zsh" ]]; then
 fi
 
 bindkey -v
+bindkey -r '^['
+bindkey -M viins '^a' vi-cmd-mode
+bindkey -M vicmd '^a' vi-insert
 
 bindkey '^[[Z' reverse-menu-complete
 bindkey '^f' autosuggest-accept
