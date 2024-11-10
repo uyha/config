b3998a8d58d3d608320d8c754f51ec8cae5a8e21
Uy Ha <hchanuy@gmail.com>
2024-11-10 09:32:57 +0000

    fix(nvim): actually ensure that `cmake` tree-sitter parser is installed

--- nvim/lua/plugins/cmake.lua
+++ nvim/lua/plugins/cmake.lua
@@ -1,11 +1,11 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = {
-      servers = {
-        cmake = {},
-      },
-    },
+    opts = function(_, opts)
+      if type(opts.ensure_installed) ~= "table" then return end
+
+      vim.list_extend(opts.ensure_installed, { "cmake" })
+    end,
   },
   {
     "neovim/nvim-lspconfig",
