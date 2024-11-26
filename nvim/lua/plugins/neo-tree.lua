ee9781c0b6829ad0f8e669c0c11560a2e1c74b57
Uy Ha <hchanuy@gmail.com>
2024-11-26 11:04:39 +0000

    feat(nvim): ignore `noice` windows when picking

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -10,7 +10,7 @@ return {
           include_current_win = false,
           autoselect_one = true,
           bo = {
-            filetype = { "neo-tree", "neo-tree-popup", "snacks_notif" },
+            filetype = { "neo-tree", "neo-tree-popup", "snacks_notif", "noice" },
             buftype = { "terminal", "quickfix" },
           },
         },
