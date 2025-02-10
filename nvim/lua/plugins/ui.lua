18dd9a3f7341fb96cb5009c81e5e27de05647c39
Uy Ha <hchanuy@gmail.com>
2025-02-10 22:09:10 +0000

    feat(nvim): remap closing buffer keys to match movement keys

---
+++ nvim/lua/plugins/ui.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    keys = {
+      { "<leader>bl", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
+      { "<leader>bh", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
+    },
+  },
+}
