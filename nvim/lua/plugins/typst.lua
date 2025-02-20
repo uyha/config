f2fc0f88f4e2df2dca45ba6db61e8609c93b8901
Uy Ha <hchanuy@gmail.com>
2025-02-20 09:44:35 +0000

    feat(nvim): add typst support

---
+++ nvim/lua/plugins/typst.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) ~= "table" then return end
+
+      vim.list_extend(opts.ensure_installed, { "typst" })
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) ~= "table" then return end
+
+      vim.list_extend(opts.ensure_installed, { "tinymist" })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tinymist = {},
+      },
+    },
+  },
+}
