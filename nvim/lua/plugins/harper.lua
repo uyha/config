9ef595ec4e07b13641d5d9fa647b7be898d656d7
Uy Ha <hchanuy@gmail.com>
2025-03-22 19:53:54 +0000

    feat(nvim): add harper for spell and grammar checking

---
+++ nvim/lua/plugins/harper.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        harper_ls = {},
+      },
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) ~= "table" then return end
+
+      vim.list_extend(opts.ensure_installed, {
+        "harper-ls",
+      })
+    end,
+  },
+}
