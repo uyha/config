5d18179cec7e9704c97d9c4f8ed58497e19c440b
Uy Ha <hchanuy@gmail.com>
2025-03-25 08:47:51 +0000

    feat(nvim): enable `nvim-treesitter-textobjects`

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -21,6 +21,28 @@ return {
           "yaml",
         })
       end
+
+      opts.textobjects = vim.tbl_deep_extend("force", opts.textobjects or {}, {
+        select = {
+          enable = true,
+          lookahead = true,
+        },
+        swap = {
+          enable = true,
+          swap_next = { ["<leader>tl"] = "@parameter.inner" },
+          swap_previous = { ["<leader>th"] = "@parameter.inner" },
+        },
+        move = {
+          enable = true,
+          set_jumps = true,
+          goto_next_start = {
+            ["]a"] = "@parameter.inner",
+          },
+          goto_previous_start = {
+            ["[a"] = "@parameter.inner",
+          },
+        },
+      })
     end,
   },
 }
