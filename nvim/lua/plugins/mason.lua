8c8fcf7454d1cdbb9e0223b3b7687827c0d6a684
Uy Ha <hchanuy@gmail.com>
2025-05-07 09:10:37 +0000

    fix(nvim): pin mason and mason-lspconfig to working versions

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,16 +1,20 @@
 return {
-  "williamboman/mason.nvim",
-  opts = function(_, opts)
-    opts.registries = {
-      "lua:bearmason",
-      "github:mason-org/mason-registry",
-    }
-    if type(opts.ensure_installed) == "table" then
-      vim.list_extend(opts.ensure_installed, {
-        -- Linters
-        "actionlint",
-      })
-    end
-    opts.PATH = "skip"
-  end,
+  {
+    "williamboman/mason.nvim",
+    version = "1.11.0",
+    opts = function(_, opts)
+      opts.registries = {
+        "lua:bearmason",
+        "github:mason-org/mason-registry",
+      }
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, {
+          -- Linters
+          "actionlint",
+        })
+      end
+      opts.PATH = "skip"
+    end,
+  },
+  { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
 }
