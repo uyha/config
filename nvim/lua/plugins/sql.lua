71c559efe84a6a5d9d32eb3efb029b171b2f04c3
Uy Ha <hchanuy@gmail.com>
2025-05-31 20:26:52 +0000

    feat(nvim): stop using sqlfluff

--- nvim/lua/plugins/sql.lua
+++ nvim/lua/plugins/sql.lua
@@ -7,30 +7,4 @@ return {
       vim.list_extend(opts.ensure_installed, { "sql" })
     end,
   },
-  {
-    "mason-org/mason.nvim",
-    opts = function(_, opts)
-      if type(opts.ensure_installed) ~= "table" then return end
-
-      vim.list_extend(opts.ensure_installed, {
-        "sqlfluff",
-      })
-    end,
-  },
-  {
-    "nvimtools/none-ls.nvim",
-    opts = function(_, opts)
-      if type(opts.sources) ~= "table" then return end
-
-      local nls = require("null-ls")
-      local builtins = nls.builtins
-      local formatting = builtins.formatting
-      local diagnostics = builtins.diagnostics
-
-      vim.list_extend(opts.sources, {
-        formatting.sqlfluff.with({ timeout = 5000 }),
-        diagnostics.sqlfluff,
-      })
-    end,
-  },
 }
