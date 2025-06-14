1f450fb7c18526c29a959e889e8cc7e9c95ecaec
Uy Ha <hchanuy@gmail.com>
2025-06-14 18:32:56 +0300

    feat(zsh): manually manage plugins now

---
+++ zsh/conf.d/interactive/004-plugins.zsh
@@ -0,0 +1,18 @@
+add-plugin() {
+  url=$1
+  name=$(basename "$url")
+  dir=$XDG_DATA_HOME/zsh-plugins/$name
+
+  if ! [[ -d "$dir" ]]; then
+    git clone "$url" "$dir"
+  fi
+}
+
+add-plugin "https://github.com/Aloxaf/fzf-tab"
+add-plugin "https://github.com/zsh-users/zsh-autosuggestions"
+add-plugin "https://github.com/zsh-users/zsh-syntax-highlighting"
+add-plugin "https://github.com/zsh-users/zsh-completions"
+
+for script in $XDG_DATA_HOME/zsh-plugins/*/*.plugin.zsh; do
+  source "$script"
+done
--- zsh/config.zsh
+++ zsh/config.zsh
@@ -7,6 +7,9 @@ for f in "$HOME"/.config/zsh/conf.d/global/*; do
 done
 
 if [[ $- == *i* ]]; then
+  autoload -Uz compinit
+  compinit
+
   for f in "$HOME"/.config/zsh/conf.d/interactive/*; do
     source "$f"
   done
