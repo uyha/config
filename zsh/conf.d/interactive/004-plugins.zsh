f9980dd737bf1c7ea236ba1efd8096b4b0ca7f03
Uy Ha <hchanuy@gmail.com>
2025-09-27 21:08:43 +0300

    feat(zsh): add vi mode plugin

--- zsh/conf.d/interactive/004-plugins.zsh
+++ zsh/conf.d/interactive/004-plugins.zsh
@@ -12,6 +12,7 @@ add-plugin "https://github.com/Aloxaf/fzf-tab"
 add-plugin "https://github.com/zsh-users/zsh-autosuggestions"
 add-plugin "https://github.com/zsh-users/zsh-syntax-highlighting"
 add-plugin "https://github.com/zsh-users/zsh-completions"
+add-plugin "https://github.com/jeffreytse/zsh-vi-mode"
 
 for script in $XDG_DATA_HOME/zsh-plugins/*/*.plugin.zsh; do
   source "$script"
