c19bca59457da64b5ff0fdd3d3db76c416ac90c7
Uy Ha <hchanuy@gmail.com>
2025-10-08 15:38:36 +0300

    feat(nvim): enable reStructuredText tree-sitter grammar

--- nvim/lua/plugins/sphinx.lua
+++ nvim/lua/plugins/sphinx.lua
@@ -1,18 +1,9 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      if type(opts.ensure_installed) ~= "table" then return end
-
-      -- The tree-sitter parser for RST is incomplete, hence removing it to be less
-      -- annoying
-      for index, value in ipairs(opts.ensure_installed) do
-        if value == "rst" then
-          table.remove(opts.ensure_installed, index)
-          break
-        end
-      end
-    end,
+    opts = {
+      ensure_installed = { "rst" },
+    },
   },
   {
     "neovim/nvim-lspconfig",
@@ -20,12 +11,6 @@ return {
       servers = {
         esbonio = {},
       },
-    },
-  },
-  {
-    "mason-org/mason.nvim",
-    opts = {
-      ensure_installed = { "esbonio" },
     },
   },
 }
