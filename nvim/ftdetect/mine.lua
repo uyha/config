9ff9bcd7023053792156b314fbd7adcca2e82dff
Uy Ha <hchanuy@gmail.com>
2025-10-31 11:53:19 +0200

    feat(nvim): detect sshdconfig file

--- nvim/ftdetect/mine.lua
+++ nvim/ftdetect/mine.lua
@@ -33,8 +33,9 @@ vim.filetype.add({
   pattern = {
     [".*/include/.*.inc"] = "bitbake",
     [".*/.config/git/.*"] = "gitconfig",
-    ["nginx/*.conf"] = "nginx",
+    ["nginx/.*.conf"] = "nginx",
     ["justfile"] = "just",
+    [".*/sshd/.*.conf"] = "sshdconfig",
     [".*/.*"] = function(_, bufnr)
       local content = vim.api.nvim_buf_get_lines(bufnr, 0, 1, false)[1] or ""
       if vim.regex([[^#!.*uv run python]]):match_str(content) ~= nil then return "python" end
