0c701547d2baae92a181c2342aed733ffa9fbb7c
Uy Ha <hchanuy@gmail.com>
2025-11-13 23:50:22 +0200

    remove colorful-winsep due to bugginess

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -26,9 +26,4 @@ return {
       { "<leader>bh", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
     },
   },
-  {
-    "nvim-zh/colorful-winsep.nvim",
-    config = true,
-    event = { "WinLeave" },
-  },
 }
