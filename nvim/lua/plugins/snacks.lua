43ace283470b87bfad6389814b5a21e7f2967401
Uy Ha <hchanuy@gmail.com>
2025-11-14 08:56:45 +0200

    feat(nvim): add back `<C-g>` to move between file and explorer

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -15,6 +15,7 @@ return {
             win = {
               list = {
                 keys = {
+                  ["<C-g>"] = "confirm",
                   ["<C-l>"] = { function() vim.cmd([[wincmd l]]) end },
                   ["z"] = "explorer_close_all",
                 },
@@ -25,6 +26,19 @@ return {
       },
     },
     keys = {
+      {
+        "<C-g>",
+        function()
+          local Snacks = require("snacks")
+          local explorers = Snacks.picker.get({ source = "explorer" })
+          if #explorers == 0 then
+            Snacks.explorer()
+          else
+            explorers[1]:focus()
+          end
+        end,
+        desc = "Find files (including hidden)",
+      },
       {
         "<leader>fa",
         function() require("snacks").picker.files({ hidden = true }) end,
