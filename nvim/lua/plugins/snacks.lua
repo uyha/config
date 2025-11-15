f19160838620eadc66bdfca9d6eef4bdfc49c982
Uy Ha <hchanuy@gmail.com>
2025-11-15 14:04:04 +0200

    feat(nvim): customize `Snacks`
    <C-g>, when in a file, focuses the file in the explorer
    <C-g>, when in the explorer, opens the file and closes the explorer.
    
    <leader>fe and <leader>fE are removed.
    <leader>e opens the explorer in cwd.
    <leader>E open the root directory.

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -15,9 +15,14 @@ return {
             win = {
               list = {
                 keys = {
-                  ["<C-g>"] = "confirm",
+                  ["<C-g>"] = function()
+                    local explorers = Snacks.picker.get({ source = "explorer" })
+                    if #explorers ~= 0 then
+                      explorers[1]:action("confirm")
+                      explorers[1]:close()
+                    end
+                  end,
                   ["<C-l>"] = { function() vim.cmd([[wincmd l]]) end },
-                  ["z"] = "explorer_close_all",
                 },
               },
             },
@@ -26,26 +31,37 @@ return {
       },
     },
     keys = {
+      { "<leader>fe", false },
+      { "<leader>fE", false },
+      {
+        "<leader>e",
+        function() Snacks.explorer() end,
+        desc = "Explorer Snacks (cwd)",
+      },
+      {
+        "<leader>E",
+        function() Snacks.explorer({ cwd = LazyVim.root() }) end,
+        desc = "Explorer Snacks (root dir)",
+      },
       {
         "<C-g>",
         function()
-          local Snacks = require("snacks")
           local explorers = Snacks.picker.get({ source = "explorer" })
           if #explorers == 0 then
-            Snacks.explorer()
+            Snacks.explorer.reveal()
           else
             explorers[1]:focus()
           end
         end,
+        desc = "Focus file",
+      },
+      {
+        "<leader>fh",
+        function() Snacks.picker.files({ hidden = true }) end,
         desc = "Find files (including hidden)",
       },
       {
         "<leader>fa",
-        function() require("snacks").picker.files({ hidden = true }) end,
-        desc = "Find files (including hidden)",
-      },
-      {
-        "<leader>fi",
         function() require("snacks").picker.files({ hidden = true, ignored = true }) end,
         desc = "Find all files",
       },
