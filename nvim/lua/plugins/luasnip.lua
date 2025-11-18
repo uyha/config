99d8fd44b7f8040d95418080f0e7abcdd03c4e43
Uy Ha <hchanuy@gmail.com>
2025-11-18 10:25:47 +0200

    feat(nvim): add juicy main snippet for zig

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -1406,14 +1406,30 @@ return {
         )
       ),
       s(
-        "main",
+        "jmain",
         fmt(
           [[
-          pub fn main() !void {{
-            {}
+          fn juicyMain(gpa: Allocator, io: Io) !void {{
+            _ = &gpa;
+            _ = &io;
           }}
+          pub fn main() !void {{
+            var allocator : std.heap.DebugAllocator(.{{}}) = .init;
+            defer _ = allocator.deinit();
+            const gpa = allocator.allocator();
+
+            var runtime: std.Io.Threaded = .init(gpa);
+            defer runtime.deinit();
+
+            try juicyMain(gpa, runtime.io());
+          }}
+
+          const std = @import("std");
+          const Allocator = std.mem.Allocator;
+          const Io = std.Io;
+          const Clock = Io.Clock;
           ]],
-          { i(0) }
+          {}
         )
       ),
       s(
