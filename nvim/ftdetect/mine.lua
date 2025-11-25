ed0e14cdacaf7a11d9164474d32071ea16eb6e2b
Uy Ha <hchanuy@gmail.com>
2025-11-25 22:41:49 +0200

    stop interpretting jinja file as another file

--- nvim/ftdetect/mine.lua
+++ nvim/ftdetect/mine.lua
@@ -16,16 +16,6 @@ vim.filetype.add({
       return vim.filetype.match({ filename = captured })
     end,
 
-    ---@param path string
-    ---@return string | nil
-    jinja = function(path)
-      local _, _, filename = path:find(".*/(.*)$")
-      local _, _, captured = filename:find("%.(.*).jinja$")
-      if captured == "hpp" then return "cpp" end
-
-      return captured
-    end,
-
     zsh = "bash",
     eds = "eds",
     dcf = "eds",
