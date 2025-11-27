c8e3c635154ad3d8b0dcc995c3392b198e68cead
Uy Ha <hchanuy@gmail.com>
2025-11-27 22:01:46 +0100

    feat(zsh): source only files ending with .zsh

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -2,12 +2,12 @@ is-tmux() {
   [[ -n "$TMUX" ]]
 }
 
-for f in "$HOME"/.config/zsh/conf.d/global/*; do
+for f in "$HOME"/.config/zsh/conf.d/global/*.zsh; do
   source "$f"
 done
 
 if [[ $- == *i* ]]; then
-  for f in "$HOME"/.config/zsh/conf.d/interactive/*; do
+  for f in "$HOME"/.config/zsh/conf.d/interactive/*.zsh; do
     source "$f"
   done
 
