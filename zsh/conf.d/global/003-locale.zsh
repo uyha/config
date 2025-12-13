aedfc6839c341e023e325ae0f8c12af956a8c552
Uy Ha <hchanuy@gmail.com>
2025-12-13 14:12:36 +0100

    fix(nvim): stop perl from complaining about locale

--- zsh/conf.d/global/003-locale.zsh
+++ zsh/conf.d/global/003-locale.zsh
@@ -1,6 +1,6 @@
 declare -A locales
-locales[ubuntu-20.04]=en_US.UTF-8
-locales[ubuntu-24.04]=en_GB.UTF-8
+locales["ubuntu-20.04"]=en_US.UTF-8
+locales["ubuntu-24.04"]=en_US.UTF-8
 
 distro=
 if [[ "$(lsb_release -d)" =~ Ubuntu ]]; then
@@ -8,6 +8,6 @@ if [[ "$(lsb_release -d)" =~ Ubuntu ]]; then
 fi
 distro_version="$(lsb_release -r | awk '{print $2}' | xargs)"
 
-if [[ -n "${locales[$distro-$distro_version]}" ]]; then
-  export LC_ALL="${locales[$distro-$distro_version]}"
+if [[ -n "${locales["$distro-$distro_version"]}" ]]; then
+  export LC_ALL="${locales["$distro-$distro_version"]}"
 fi
